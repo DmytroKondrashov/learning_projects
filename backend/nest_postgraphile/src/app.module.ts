d99831064b15f6298ca9ebe1441ecd7e02a6cdee
DmytroKondrashov <kondrashovd101@gmail.com>
2024-10-02 20:32:43 +0300

    add TypeOrmModule

--- backend/nest_postgraphile/src/app.module.ts
+++ backend/nest_postgraphile/src/app.module.ts
@@ -2,9 +2,22 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PostModule } from './post/post.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [PostModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'nest_postgraphile',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: false,
+    }),
+    PostModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
