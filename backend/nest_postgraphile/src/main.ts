3eb1014beb81f0863b1050d128d731b7deacc252
DmytroKondrashov <kondrashovd101@gmail.com>
2024-10-03 22:07:38 +0300

    integrate postgraphile

--- backend/nest_postgraphile/src/main.ts
+++ backend/nest_postgraphile/src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { PostgraphileService } from './postgraphile/postgraphile.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const postgraphileService = app.get(PostgraphileService);
+  await postgraphileService.onModuleInit();
+  app.use(postgraphileService.postgraphileMiddleware);
   await app.listen(3000);
 }
 bootstrap();
--- backend/nest_postgraphile/src/postgraphile/postgraphile.service.ts
+++ backend/nest_postgraphile/src/postgraphile/postgraphile.service.ts
@@ -1,4 +1,21 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, OnModuleInit } from '@nestjs/common';
+import { postgraphile } from 'postgraphile';
 
 @Injectable()
-export class PostgraphileService {}
+export class PostgraphileService implements OnModuleInit {
+  public postgraphileMiddleware: any;
+
+  onModuleInit() {
+    this.postgraphileMiddleware = postgraphile(
+      'postgres://postgres:root@localhost/nest_postgraphile',
+      'public',
+      {
+        watchPg: true,
+        graphiql: true,
+        enhanceGraphiql: true,
+        dynamicJson: true,
+        ignoreRBAC: false,
+      },
+    );
+  }
+}
