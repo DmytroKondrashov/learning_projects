c7bf2454655ac025276a3ff79e992caf4c6f6887
DmytroKondrashov <kondrashovd101@gmail.com>
2024-10-06 19:14:12 +0300

    remove unneeded fields

--- backend/nest_postgraphile/src/user/entities/user.entity.ts
+++ backend/nest_postgraphile/src/user/entities/user.entity.ts
@@ -5,17 +5,8 @@ export class User {
   @PrimaryGeneratedColumn()
   id: number;
 
-  @Column({ type: 'varchar', length: 30 })
-  name: string;
-
-  @Column({ type: 'varchar', length: 15 })
-  username: string;
-
   @Column({ type: 'varchar', length: 40 })
   email: string;
-
-  @Column({ type: 'int' })
-  age: number;
 
   @Column({ type: 'varchar' })
   password: string;
