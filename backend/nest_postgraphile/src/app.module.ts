adac35c36d1bf7a251486c8b6f4699e5e3dd90cf
DmytroKondrashov <kondrashovd101@gmail.com>
2024-10-06 17:52:22 +0300

    remove all db related configs

--- backend/nest_postgraphile/src/app.module.ts
+++ backend/nest_postgraphile/src/app.module.ts
@@ -2,28 +2,17 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PostModule } from './post/post.module';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserModule } from './user/user.module';
-import { DatabaseModule } from './database/database.module';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { PostgraphileService } from './postgraphile/postgraphile.service';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'postgres',
-      database: 'nest_postgraphile',
-      entities: [__dirname + '/**/*.entity{.ts,.js}'],
-      synchronize: false,
+    ConfigModule.forRoot({
+      isGlobal: true,
     }),
     PostModule,
     UserModule,
-    DatabaseModule,
-    ConfigModule,
   ],
   controllers: [AppController],
   providers: [AppService, ConfigService, PostgraphileService],
