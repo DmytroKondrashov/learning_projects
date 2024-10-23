d19499f08c57f7bea016d016edf15fe5812b1207
DmytroKondrashov <kondrashovd101@gmail.com>
2024-10-23 10:19:14 +0300

    ad commentst

--- basics/js/asynchronous/index.js
+++ basics/js/asynchronous/index.js
@@ -1,7 +1,10 @@
+// Will be executed after 1 second, the callback function will be sent to the event loop
 window.setTimeout(() => {
     console.log('Hello');
 }, 1000);
 
+// Will be executed after 0 seconds, the callback function will be sent to the event loop
 setTimeout(() => console.log('Test'), 0);
 
+// Will be executed immediately
 console.log('World');
