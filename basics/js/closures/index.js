38f87116e61055df7a689c9f9530768b7a0524bd
DmytroKondrashov <kondrashovd101@gmail.com>
2024-10-23 09:30:25 +0300

    add closures learning material

---
+++ basics/js/closures/index.js
@@ -0,0 +1,21 @@
+// basic closure example
+// function createCalcFunction(n) {
+//   return function() {
+//     console.log(1000 * n)
+//   }
+// }
+
+// const calc = createCalcFunction(42);
+// calc();
+
+function createIncrementor(n) {
+  return function(num) {
+    return n + num;
+  }
+}
+
+const addOne = createIncrementor(1);
+const addTen = createIncrementor(10);
+
+console.log(addOne(10));
+console.log(addTen(10));
