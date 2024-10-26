92cc9edfbaa8c68640ca348699178cc7c0423eac
DmytroKondrashov <kondrashovd101@gmail.com>
2024-10-26 11:23:54 +0300

    add serving static files functionality

--- backend/express/basic_app/index.js
+++ backend/express/basic_app/index.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const path = require('path')
 const app = express();
 const port = 3000;
+
+// use this middleware to serve the static files like /images/cat.avif
+app.use(express.static(path.join(__dirname,'public')))
 
 app.get('/', (req, res) => {
   res.send('Hello world')
