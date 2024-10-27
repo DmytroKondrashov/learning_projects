2b6fd52fcb8263fc7ea9e058c487b3174bdf2316
DmytroKondrashov <kondrashovd101@gmail.com>
2024-10-27 17:49:38 +0200

    add authentication functionality

--- backend/express/basic_app/index.js
+++ backend/express/basic_app/index.js
@@ -35,7 +35,7 @@ app.use(function(req, res, next) {
 })
 
 app.get('/', (req, res) => {
-  res.send('Hello world')
+  res.redirect('/login');
 })
 
 app.post('/', (req, res) => {
@@ -51,8 +51,81 @@ app.delete('/user', (req, res) => {
 })
 
 // ================================ authentication ==========================================
+// users "database"
+let users = {
+  tj: { name: "tj" }
+}
 
+// when you create a user, generate a salt
+// and hash the password ('foobar' is the pass here)
+hash({ password: 'foobar' }, function (err, pass, salt, hash) {
+  if (err) throw err;
+  users.tj.salt = salt;
+  users.tj.hash = hash;
+})
 
+function authenticate(name, pass, fn) {
+  if(!module.parent) console.log('authenticating %s:%s', name, pass);
+  let user = users[name];
+  if (! user) return fn(null, null);
+  // apply the same algorithm to the POSTed password, applying
+  // the hash against the pass / salt, if there is a match we
+  // found the user
+  hash({ password: pass, salt: user.salt }, function(err, pass, salt, hash) {
+    if (err) return fn(err);
+    if (hash === user.hash) return fn(null, user);
+    fn(null, null);
+  })
+}
+
+function restrict(req, res, next) {
+  if (req.session.user) {
+    next();
+  } else {
+    req.session.error = 'Access denied';
+    res.redirect('/login');
+  }
+}
+
+app.get('/restricted', restrict, function(req, res) {
+  res.send('Welcome to the secret area! Click <a href="/logout">here</a> to logout.')
+})
+
+app.get('/login', function(req, res) {
+  res.render('login');
+})
+
+app.get('/logout', function(req, res) {
+  req.session.destroy(function() {
+    res.redirect('/login');
+  })
+})
+
+app.post('/login', function(req, res) {
+  if (!req.body) return res.sendStatus(400)
+    authenticate(req.body.username, req.body.password, function(err, user){
+      if (err) return next(err)
+      if (user) {
+        // Regenerate session when signing in
+        // to prevent fixation
+        req.session.regenerate(function(){
+          // Store the user's primary key
+          // in the session store to be retrieved,
+          // or in this case the entire user object
+          req.session.user = user;
+          req.session.success = 'Authenticated as ' + user.name
+            + ' click to <a href="/logout">logout</a>. '
+            + ' You may now access <a href="/restricted">/restricted</a>.';
+          res.redirect(req.get('Referrer') || '/');
+        });
+      } else {
+        req.session.error = 'Authentication failed, please check your '
+          + ' username and password.'
+          + ' (use "tj" and "foobar")';
+        res.redirect('/login');
+    }
+  });
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
