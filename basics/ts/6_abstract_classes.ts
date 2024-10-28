f0ddf1faae486a90891432230f56e547535ac7af
DmytroKondrashov <kondrashovd101@gmail.com>
2024-10-28 21:37:54 +0200

    add ts abstract classes learning materials

---
+++ basics/ts/6_abstract_classes.ts
@@ -0,0 +1,14 @@
+abstract class Component {
+  abstract render(): void;
+  abstract info(): string;
+}
+
+class AppComponent extends Component {
+  render(): void {
+    console.log("Component on render");
+  }
+
+  info(): string {
+    return "This is info";
+  }
+}
