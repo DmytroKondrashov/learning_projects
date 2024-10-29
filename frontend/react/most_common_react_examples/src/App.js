905959df732950db0e707174eb80a38b02e1be68
DmytroKondrashov <kondrashovd101@gmail.com>
2024-10-29 11:33:16 +0200

    add object as state example

--- frontend/react/most_common_react_examples/src/App.js
+++ frontend/react/most_common_react_examples/src/App.js
@@ -1,12 +1,13 @@
 import Toolbar from './components/toolbar'
 import './App.css';
-import UseStateComponent from './components/use_state_component';
+import {UseStateComponent, Person} from './components/use_state_component';
 
 function App() {
   return (
     <>
       <Toolbar />
       <UseStateComponent initialCount={3} />
+      <Person />
     </>
   );
 }
--- frontend/react/most_common_react_examples/src/components/use_state_component.jsx
+++ frontend/react/most_common_react_examples/src/components/use_state_component.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-export default function UseStateComponent({ initialCount }) {
+export function UseStateComponent({ initialCount }) {
   const [number, setNumber] = useState(initialCount);
 
   return(
@@ -14,5 +14,30 @@ export default function UseStateComponent({ initialCount }) {
         setNumber(n => n + 1);
       }}>+3</button>
     </div>
+  )
+}
+
+export function Person() {
+  const [person, setPerson] = useState({
+    name: 'John',
+    age: 30
+  })
+
+  return(
+    <>
+      <p>
+          {person.name} is {person.age} years old.
+      </p>
+      <label htmlFor="name">Name:
+        <input id="name" value={person.name} onChange={e => {
+          setPerson({...person, name: e.target.value})
+        }} />
+      </label>
+      <label htmlFor="age">Age:
+        <input id="age" value={person.age} onChange={e => {
+          setPerson({...person, age: e.target.value})
+        }}/>
+      </label>
+    </>
   )
 }
