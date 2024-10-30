d0db16c496da5ddf056c62bd857995f689c3b73d
DmytroKondrashov <kondrashovd101@gmail.com>
2024-10-30 18:59:40 +0200

    add new methods to linked list

--- basics/data_structures/02_linked_list.js
+++ basics/data_structures/02_linked_list.js
@@ -30,6 +30,100 @@ class LinkedList {
     this.size++;
   }
 
+  append(value) {
+    const node = new Node(value);
+    if (this.isEmpty()) {
+      this.head = node;
+    } else {
+      let current = this.head;
+      while (current.next) {
+        current = current.next;
+      }
+      current.next = node;
+    }
+    this.size++;
+  }
+
+  insert(value, index) {
+    if (index < 0 || index > this.size) {
+      return;
+    }
+    if (index === 0) {
+      this.prepend(value);
+    } else {
+      const node = new Node(value);
+      let prev = this.head;
+      for (let i = 0; i < index - 1; i++) {
+        prev = prev.next;
+      }
+      node.next = prev.next;
+      prev.next = node;
+    }
+    this.size++;
+  }
+
+  remove(index) {
+    if (index < 0 || index >= this.size) {
+      return null;
+    }
+    let removedNode;
+    if (index === 0) {
+      removedNode = this.head;
+      this.head = this.head.next;
+      this.size--;
+      return removedNode.value;
+    } else {
+      let prev = this.head;
+      for (let i = 0; i < index - 1; i++) {
+        prev = prev.next;
+      }
+      removeNode = prev.next;
+      prev.next = removedNode.next;
+      this.size--;
+      return removedNode.value;
+    }
+  }
+
+  removeValue(value) {
+    if (this.isEmpty()) {
+      return null;
+    }
+    let removedNode = this.head;
+    if (removedNode.value === value) {
+      this.head = removedNode.next;
+      this.size--;
+      return removedNode.value;
+    } else {
+      let prev = this.head;
+      while (prev.next && prev.next.value !== value) {
+        prev = prev.next;
+      }
+      if (prev.next) {
+        removedNode = prev.next;
+        prev.next = removedNode.next;
+        this.size--;
+        return removedNode.value;
+      }
+    }
+    return null;
+  }
+
+  search(value) {
+    if (this.isEmpty()) {
+      return -1;
+    }
+    let i = 0;
+    let current = this.head;
+    while (current) {
+      if (current.value === value) {
+        return i;
+      }
+      current = current.next;
+      i++;
+    }
+    return -1;
+  }
+
   print() {
     if (this.isEmpty()) {
       console.log('List is empty');
