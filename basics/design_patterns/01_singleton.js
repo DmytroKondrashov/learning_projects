edc4747cfeb774c77152bdef47b10cfb0049feef
DmytroKondrashov <kondrashovd101@gmail.com>
2024-10-31 23:00:39 +0200

    add additional singleton

--- basics/design_patterns/01_singleton.js
+++ basics/design_patterns/01_singleton.js
@@ -1,28 +1,83 @@
+// class Singleton {
+//   // JS private property
+//   static #instance = null;
+
+//   constructor() {
+//     // Check if instance already exists, but do we really need it?
+//     // if (Singleton.#instance instanceof Singleton) {
+//     //   return Singleton.#instance;
+//     // }
+//     this.count = 0;
+//     Singleton.#instance = this;
+//     return this;
+//   }
+
+//   getCount() {
+//     return this.count;
+//   }
+
+//   increment() {
+//     this.count++;
+//   }
+
+//   static getInstance(){
+//     if (!Singleton.#instance) {
+//       Singleton.#instance = new Singleton();
+//     }
+//     return Singleton.#instance;
+//   }
+// }
+
+// const singleton1 = Singleton.getInstance();
+// const singleton2 = Singleton.getInstance();
+
+// // Will throw an error
+// // const test = new Singleton();
+// // test.#instance = [];
+
+// // Will throw an error
+// // singleton1.#instance = [];
+
+// console.log(singleton1 === singleton2);
+
+
+
+
+
+
+
+
 class Singleton {
-  // JS private property
-  #instance = null;
+  static #instance = null;
+
   constructor() {
-    // more strict check!!!
-    if (typeof Singleton.instance === "object") {
-      return Singleton.instance;
+    if (Singleton.#instance) {
+      return Singleton.#instance;
     }
-    this.count = 0;
-    Singleton.instance = this;
-    return this;
+
+    this.name = '';
+    Singleton.#instance = this;
   }
 
-  getCount() {
-    return this.count;
+  getName() {
+    return this.name;
   }
 
-  increment() {
-    this.count++;
+  setName(name) {
+    this.name = name;
+  }
+
+  static getInstance() {
+    if (Singleton.#instance) {
+      return Singleton.#instance;
+    }
+    Singleton.#instance = new Singleton();
+    return Singleton.#instance;
   }
 }
 
-const singleton1 = new Singleton();
-const singleton2 = new Singleton();
+const sing1 = Singleton.getInstance();
+const sing2 = Singleton.getInstance();
 
-singleton1.instance = []
-
-console.log(singleton1 === singleton2);
+sing1.setName("Ilon");
+console.log(sing2.getName());
