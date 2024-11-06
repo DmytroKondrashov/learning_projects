877edc68d967fe280bca8a9940519b2e1845a486
DmytroKondrashov <kondrashovd101@gmail.com>
2024-11-06 09:54:03 +0200

    implement stack using classes

--- basics/data_structures/06_stack.js
+++ basics/data_structures/06_stack.js
@@ -1,9 +1,29 @@
-const stack = [];
+// using built-in methods
+// const stack = [];
 
-stack.push('a');
-stack.push('b');
-stack.push('c');
+// stack.push('a');
+// stack.push('b');
+// stack.push('c');
 
-stack.pop();
+// stack.pop();
 
-console.log(stack);
+// console.log(stack);
+
+// using classes
+class Stack {
+  constructor() {
+    this.items = [];
+  }
+
+  push(item) {
+    this.items.push(item);
+  }
+
+  pop() {
+    return this.items.pop();
+  }
+
+  peek() {
+    return this.items[this.items.length - 1];
+  }
+}
