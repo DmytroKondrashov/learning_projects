679785775fe47275ae8bf02259392f533736e701
DmytroKondrashov <kondrashovd101@gmail.com>
2024-11-10 22:06:41 +0200

    add module

--- backend/nestjs/test/src/app.module.ts
+++ backend/nestjs/test/src/app.module.ts
@@ -1,12 +1,17 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { CatsController } from './cats/cats.controller';
-import { CatsService } from './cats/cats.service';
+import { Global, Module } from '@nestjs/common';
+import { CatsModule } from './cats/cats.module';
 
+// When you want to provide a set of providers which should be available everywhere out-of-the-box - use @Global()
+@Global()
 @Module({
-  imports: [],
-  controllers: [AppController, CatsController],
-  providers: [AppService, CatsService],
+  imports: [CatsModule],
+  // We can inject dependencies directly into the app module
+  // controllers: [AppController, CatsController],
+  // providers: [AppService, CatsService],
 })
 export class AppModule {}
+
+// A module class can inject providers as well (e.g., for configuration purposes):
+// export class CatsModule {
+//   constructor(private catsService: CatsService) {}
+// }
