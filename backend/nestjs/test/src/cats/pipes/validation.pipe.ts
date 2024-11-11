5a2573a31b59d29c0ec23da9f21cd0f1d60e8139
DmytroKondrashov <kondrashovd101@gmail.com>
2024-11-11 19:47:19 +0200

    add validation pipe with schema

--- backend/nestjs/test/src/cats/pipes/validation.pipe.ts
+++ backend/nestjs/test/src/cats/pipes/validation.pipe.ts
@@ -1,8 +1,21 @@
-import { ArgumentMetadata, Injectable, PipeTransform } from '@nestjs/common';
+import {
+  ArgumentMetadata,
+  BadRequestException,
+  Injectable,
+  PipeTransform,
+} from '@nestjs/common';
+import { ZodSchema } from 'zod';
 
 @Injectable()
 export class ValidationPipe implements PipeTransform {
+  constructor(private schema: ZodSchema) {}
+
   transform(value: any, metadata: ArgumentMetadata) {
-    return value;
+    try {
+      const parsedValue = this.schema.parse(value);
+      return parsedValue;
+    } catch (error) {
+      throw new BadRequestException('Validation error!');
+    }
   }
 }
