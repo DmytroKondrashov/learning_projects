7bb2c3dd83638362ce45a5a83da75813d8d94ec0
DmytroKondrashov <kondrashovd101@gmail.com>
2024-11-11 11:17:47 +0200

    add logger middleware

--- backend/nestjs/test/src/app.module.ts
+++ backend/nestjs/test/src/app.module.ts
@@ -1,5 +1,13 @@
-import { Global, Module } from '@nestjs/common';
+import {
+  Global,
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from '@nestjs/common';
 import { CatsModule } from './cats/cats.module';
+import { LoggerMiddleware } from './middleware/logger.middleware';
+import { logger } from './middleware/functional.logger.middleware';
 
 // When you want to provide a set of providers which should be available everywhere out-of-the-box - use @Global()
 @Global()
@@ -9,7 +17,24 @@ import { CatsModule } from './cats/cats.module';
   // controllers: [AppController, CatsController],
   // providers: [AppService, CatsService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(LoggerMiddleware, logger)
+      // We can simply pass the controller
+      // .forRoutes(CatsController)
+
+      // We can also exclude some routes
+      // .exclude(
+      //   { path: 'cats', method: RequestMethod.GET },
+      //   { path: 'cats', method: RequestMethod.POST },
+      //   'cats/(.*)',
+      // )
+
+      // Or define the rotes (including wildcards)
+      .forRoutes({ path: 'cats', method: RequestMethod.GET });
+  }
+}
 
 // A module class can inject providers as well (e.g., for configuration purposes):
 // export class CatsModule {
