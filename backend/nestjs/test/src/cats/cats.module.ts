bc86d2a589e7df03639d3cb9c80ce5db5533cf91
DmytroKondrashov <kondrashovd101@gmail.com>
2024-11-12 13:04:53 +0200

    add config as a provider

--- backend/nestjs/test/src/cats/cats.module.ts
+++ backend/nestjs/test/src/cats/cats.module.ts
@@ -2,6 +2,13 @@ import { Module } from '@nestjs/common';
 import { CatsController } from './cats.controller';
 import { CatsService } from './cats.service';
 
+const configFactory = {
+  provide: 'CONFIG',
+  useFactory: () => {
+    return process.env.NODE_ENV === 'development' ? 'devConfig' : 'prodConfig';
+  },
+};
+
 @Module({
   controllers: [CatsController],
   // This is actually a shorthand for associating the token CatsService with the class CatsService:
@@ -9,6 +16,8 @@ import { CatsService } from './cats.service';
   //   provide: CatsService,
   //   useClass: CatsService,
   // },
-  providers: [CatsService],
+
+  // A provider can supply any value, including a primitive, an object, or a function.
+  providers: [CatsService, configFactory],
 })
 export class CatsModule {}
