b700bb5a782d82621f253aa4a5b9a5a9ee482bc4
DmytroKondrashov <kondrashovd101@gmail.com>
2024-11-15 19:43:38 +0200

    add validation pipe

--- backend/nestjs/test/src/main.ts
+++ backend/nestjs/test/src/main.ts
@@ -1,11 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // we can use guard globally
   // app.useGlobalGuards(new RolesGuard());
   app.enableShutdownHooks();
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
 bootstrap();
