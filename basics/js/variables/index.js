cb68c7e9aef08dcf33b95b3e656cf5b4bb99f4c2
DmytroKondrashov <kondrashovd101@gmail.com>
2024-11-17 16:39:53 +0200

    add variables examples

---
+++ basics/js/variables/index.js
@@ -0,0 +1,11 @@
+// Will throw an error 'ReferenceError: test is not defined'
+// console.log(test);
+const rest = 'test';
+
+// Will not throw an error, will log undefined
+console.log(test2);
+var test2 = 'test2';
+
+// Will throw an error 'Cannot access 'test3' before initialization'
+// console.log(test3);
+let test3 = 'test3';
