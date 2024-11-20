0915680289d05e86936b4c32e9d0d081657bca83
DmytroKondrashov <kondrashovd101@gmail.com>
2024-11-20 16:19:05 +0200

    move client to corresponding service

--- backend/nestjs/microservices/tcp/src/service-a/service-a.service.ts
+++ backend/nestjs/microservices/tcp/src/service-a/service-a.service.ts
@@ -1,16 +1,27 @@
 import { Injectable } from '@nestjs/common';
-import { ServiceBClient } from '../service-b/client';
+import {
+  ClientProxy,
+  ClientProxyFactory,
+  Transport,
+} from '@nestjs/microservices';
 
 @Injectable()
 export class ServiceAService {
-  private serviceBClient: ServiceBClient;
+  private client: ClientProxy;
 
   constructor() {
-    this.serviceBClient = new ServiceBClient();
+    this.client = ClientProxyFactory.create({
+      transport: Transport.TCP,
+      options: { host: 'localhost', port: 8877 },
+    });
+  }
+
+  sendMessage(pattern: any, data: any) {
+    return this.client.send(pattern, data);
   }
 
   async sendMessageToServiceB() {
-    const response = await this.serviceBClient.sendMessage(
+    const response = await this.sendMessage(
       { cmd: 'message' },
       { data: 'Hello from ServiceA' },
     );
