354ea1ef6f8f0dda9f8f6bfa6664b4fb1f55e29f
DmytroKondrashov <kondrashovd101@gmail.com>
2024-11-20 15:42:36 +0200

    create two separate services

--- backend/nestjs/microservices/tcp/service-a/src/app.module.ts
+++ backend/nestjs/microservices/tcp/service-a/src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { MathModule } from './math/math.module';
 
 @Module({
-  imports: [MathModule],
+  imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
