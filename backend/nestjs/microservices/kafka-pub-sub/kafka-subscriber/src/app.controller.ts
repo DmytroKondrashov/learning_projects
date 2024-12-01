11c5f8138b3388c43166715afc84c44c9b511880
DmytroKondrashov <kondrashovd101@gmail.com>
2024-12-01 18:12:17 +0200

    add Kafka to subscriber

--- backend/nestjs/microservices/kafka-pub-sub/kafka-subscriber/src/app.controller.ts
+++ backend/nestjs/microservices/kafka-pub-sub/kafka-subscriber/src/app.controller.ts
@@ -1,12 +1,13 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller } from '@nestjs/common';
 import { AppService } from './app.service';
+import { EventPattern } from '@nestjs/microservices';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @EventPattern('test-topic')
+  handleEvent(data: any) {
+    console.log('Received message:', data.value);
   }
 }
