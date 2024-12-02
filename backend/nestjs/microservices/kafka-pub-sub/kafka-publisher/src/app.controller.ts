256078bce567dc9b59e8eaef256be98bae9face0
DmytroKondrashov <kondrashovd101@gmail.com>
2024-12-02 16:13:03 +0200

    finish pub-sub functionality

--- backend/nestjs/microservices/kafka-pub-sub/kafka-publisher/src/app.controller.ts
+++ backend/nestjs/microservices/kafka-pub-sub/kafka-publisher/src/app.controller.ts
@@ -1,19 +1,33 @@
-import { Controller, Get, Inject } from '@nestjs/common';
-import { AppService } from './app.service';
-import { ClientKafka } from '@nestjs/microservices';
+import { Controller, Get, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
+import { Kafka, Producer } from 'kafkajs';
 
 @Controller()
-export class AppController {
-  constructor(
-    private readonly appService: AppService,
-    @Inject('KAFKA_SERVICE') private readonly kafkaClient: ClientKafka
-  ) {}
+export class AppController implements OnModuleInit, OnModuleDestroy {
+  kafka = new Kafka({ clientId: 'service-a', brokers: ['127.0.0.1:9092'] });
+  producer: Producer;
+
+  async onModuleInit() {
+    this.producer = this.kafka.producer();
+    await this.producer.connect();
+  }
+
+  async onModuleDestroy() {
+    await this.producer.disconnect();
+  }
 
   @Get('publish')
   async publishMessage() {
-    // const payload = { message: 'Hello from Publisher!' };
-    const payload = 'Hello from Publisher!';
-    await this.kafkaClient.emit('test-topic', payload).toPromise();
-    return 'Message published!';
+    await this.producer.send({
+      topic: 'test-topic',
+      messages: [
+        {
+          value: JSON.stringify({
+            cmd: 'test-topic',
+            data: 'Pub-Sub is working!',
+          }),
+        },
+      ],
+    });
+    return 'Message sent';
   }
 }
--- backend/nestjs/microservices/kafka-pub-sub/kafka-subscriber/src/app.controller.ts
+++ backend/nestjs/microservices/kafka-pub-sub/kafka-subscriber/src/app.controller.ts
@@ -1,10 +1,37 @@
-import { Controller } from '@nestjs/common';
-import { EventPattern } from '@nestjs/microservices';
+import { Controller, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
+import { Kafka } from 'kafkajs';
 
 @Controller()
-export class AppController {
-  @EventPattern('test-topic')
-  handleEvent(data: any) {
-    console.log('Received message:', data);
+export class AppController implements OnModuleInit, OnModuleDestroy {
+  kafka = new Kafka({
+    clientId: 'kafka-subscriber',
+    brokers: ['127.0.0.1:9092'],
+  });
+  private readonly consumer = this.kafka.consumer({ groupId: 'test-group' });
+
+  async onModuleInit() {
+    await this.consumer.connect();
+    await this.consumer.subscribe({
+      topic: 'test-topic',
+      fromBeginning: false,
+    });
+
+    await this.consumer.run({
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      eachMessage: async ({ topic, partition, message }) => {
+        console.log(`Received message: ${message.value}`);
+      },
+    });
+
+    console.log('Kafka consumer initialized and listening...');
   }
+
+  async onModuleDestroy() {
+    await this.consumer.disconnect();
+  }
+
+  // @MessagePattern('test-topic')
+  // handleMessage(@Payload() message: any) {
+  //   console.log(message);
+  // }
 }
