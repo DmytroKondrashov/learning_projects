d2a4807b1d2ea80d21d255c69aad8405dcd77f12
DmytroKondrashov <kondrashovd101@gmail.com>
2024-12-02 16:54:58 +0200

    finish second cubscriber

--- backend/nestjs/microservices/kafka-pub-sub/kafka-subscriber-2/src/app.controller.ts
+++ backend/nestjs/microservices/kafka-pub-sub/kafka-subscriber-2/src/app.controller.ts
@@ -1,12 +1,38 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
+import { Kafka } from 'kafkajs';
 
 @Controller()
-export class AppController {
-  constructor(private readonly appService: AppService) {}
+export class AppController implements OnModuleInit, OnModuleDestroy {
+  kafka = new Kafka({
+    clientId: 'kafka-subscriber-2',
+    brokers: ['127.0.0.1:9092'],
+  });
+  private readonly consumer = this.kafka.consumer({ groupId: 'test-group' });
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  async onModuleInit() {
+    await this.consumer.connect();
+    await this.consumer.subscribe({
+      topic: 'test-topic',
+      fromBeginning: false,
+    });
+
+    await this.consumer.run({
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      eachMessage: async ({ topic, partition, message }) => {
+        console.log(`Received message: ${message.value}`);
+      },
+    });
+
+    console.log('Kafka consumer initialized and listening...');
   }
+
+  async onModuleDestroy() {
+    await this.consumer.disconnect();
+  }
+
+  // This will not work!
+  // @MessagePattern('test-topic')
+  // handleMessage(@Payload() message: any) {
+  //   console.log(message);
+  // }
 }
