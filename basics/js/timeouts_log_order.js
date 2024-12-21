7b502bae2ad79c631312d742a4e6f3067898f8d4
DmytroKondrashov <kondrashovd101@gmail.com>
2024-12-21 19:26:21 +0200

    add timeouts examples

---
+++ basics/js/timeouts_log_order.js
@@ -0,0 +1,15 @@
+(function() {
+  console.log(1)
+
+  setTimeout(function() {console.log(2)}, 1000)
+
+  var prom = new Promise((res) => res(5))
+  prom.then((v) => { console.log(v) })
+
+  setTimeout(function() {console.log(3)}, 0)
+
+  console.log(4)
+})();
+
+//I said: 1, 4, 3, 5, 2
+//Correct: 1, 4, 5, 3, 2
