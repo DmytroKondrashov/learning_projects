f16f77393b2b5852ded86f82db1d10e05db74816
DmytroKondrashov <kondrashovd101@gmail.com>
2024-12-25 17:43:54 +0200

    implement microservice logic

--- backend/nestjs/microservices/kafka-circular-patterm-with-websocket/microservice/src/app.controller.ts
+++ backend/nestjs/microservices/kafka-circular-patterm-with-websocket/microservice/src/app.controller.ts
@@ -1,12 +1,15 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Inject } from '@nestjs/common';
+import { ClientKafka, EventPattern } from '@nestjs/microservices';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    @Inject('MICROSERVICE_SERVICE') private readonly client: ClientKafka,
+  ) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @EventPattern('kafka.test')
+  async handleKafkaTest(data: any) {
+    console.log(data);
+    await this.client.emit('kafka.response', { foo: 'bar' });
   }
 }
--- backend/nestjs/microservices/kafka-circular-patterm-with-websocket/microservice/src/app.module.ts
+++ backend/nestjs/microservices/kafka-circular-patterm-with-websocket/microservice/src/app.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 
 @Module({
-  imports: [],
+  imports: [
+    ClientsModule.register([
+      {
+        name: 'MICROSERVICE_SERVICE',
+        transport: Transport.KAFKA,
+        options: {
+          client: {
+            brokers: ['localhost:9092'],
+          },
+          consumer: {
+            groupId: 'microservice-consumer',
+          },
+        },
+      },
+    ]),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
