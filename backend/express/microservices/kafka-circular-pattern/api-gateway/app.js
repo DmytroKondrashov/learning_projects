cedc6269c63df052e3458d9f1950c614850988bb
DmytroKondrashov <kondrashovd101@gmail.com>
2024-12-27 17:17:40 +0200

    add manual topics creation

--- backend/express/microservices/kafka-circular-pattern/api-gateway/app.js
+++ backend/express/microservices/kafka-circular-pattern/api-gateway/app.js
@@ -1,12 +1,28 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const axios = require("axios");
-const { KafkaClient, Producer, Consumer } = require("kafka-node");
+const { KafkaClient, Producer, Consumer, Admin } = require("kafka-node");
 
 const app = express();
 app.use(bodyParser.json());
 
 const kafkaClient = new KafkaClient({ kafkaHost: "localhost:9092" });
+
+const admin = new Admin(kafkaClient);
+admin.createTopics(
+  [
+    { topic: 'user-topic', partitions: 1, replicationFactor: 1 },
+    { topic: 'response-topic', partitions: 1, replicationFactor: 1 }
+  ],
+  (err, result) => {
+    if (err) {
+      console.error('Error creating topics:', err);
+    } else {
+      console.log('Topics created or already exist:', result);
+    }
+  }
+);
+
 const producer = new Producer(kafkaClient);
 
 producer.on("ready", () => {
