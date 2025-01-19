0814742ec366ef32ccd22ae74ad9329cd7061b9a
DmytroKondrashov <kondrashovd101@gmail.com>
2025-01-19 19:07:43 +0200

    implement user controller

--- technologies/prisma/hello-world/src/user/user.controller.ts
+++ technologies/prisma/hello-world/src/user/user.controller.ts
@@ -1,4 +1,43 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { UserService } from './user.service';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Get()
+  async findAll() {
+    return this.userService.findAll();
+  }
+
+  @Get(':id')
+  async findOne(@Param('id') id: string) {
+    return this.userService.findOne(Number(id));
+  }
+
+  @Post()
+  async create(@Body() data: { name: string; email: string }) {
+    return this.userService.create(data);
+  }
+
+  @Put(':id')
+  async update(
+    @Param('id') id: string,
+    @Body() data: { name?: string; email?: string },
+  ) {
+    return this.userService.update(Number(id), data);
+  }
+
+  @Delete(':id')
+  async delete(@Param('id') id: string) {
+    return this.userService.delete(Number(id));
+  }
+}
