ddbfb3947ae3795a8d746431c9a2c7e8bd952043
DmytroKondrashov <kondrashovd101@gmail.com>
2025-01-19 19:06:15 +0200

    finish user service

--- technologies/prisma/hello-world/src/user/user.service.ts
+++ technologies/prisma/hello-world/src/user/user.service.ts
@@ -1,4 +1,27 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async findAll() {
+    return this.prisma.user.findMany();
+  }
+
+  async findOne(id: number) {
+    return this.prisma.user.findUnique({ where: { id } });
+  }
+
+  async create(data: { name: string; email: string }) {
+    return this.prisma.user.create({ data });
+  }
+
+  async update(id: number, data: { name?: string; email?: string }) {
+    return this.prisma.user.update({ where: { id }, data });
+  }
+
+  async delete(id: number) {
+    return this.prisma.user.delete({ where: { id } });
+  }
+}
