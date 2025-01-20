02f3ade3aec64a12edac69b26a27164b6d232615
DmytroKondrashov <kondrashovd101@gmail.com>
2025-01-20 12:23:53 +0200

    finish user service

--- technologies/graphql/nest_graphql/src/user/user.service.ts
+++ technologies/graphql/nest_graphql/src/user/user.service.ts
@@ -1,4 +1,29 @@
 import { Injectable } from '@nestjs/common';
+import { User } from './entities/user.entity';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  private users: User[] = [
+    { id: 1, name: 'John Doe', age: 30, email: 'john.doe@example.com' },
+    { id: 2, name: 'Jane Smith', age: 25, email: 'jane.smith@example.com' },
+  ];
+
+  findAll() {
+    return this.users;
+  }
+
+  findOne(id: number): User {
+    return this.users.find((user) => user.id === id);
+  }
+
+  create(user: Partial<User>): User {
+    const newUser: User = {
+      id: this.users.length + 1,
+      name: user.name,
+      age: user.age,
+      email: user.email,
+    };
+    this.users.push(newUser);
+    return newUser;
+  }
+}
