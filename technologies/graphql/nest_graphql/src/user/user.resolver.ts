f857363444c28fd015ce2223b2ace69ab73e7cb5
DmytroKondrashov <kondrashovd101@gmail.com>
2025-01-20 12:30:49 +0200

    finish the resolver

--- technologies/graphql/nest_graphql/src/user/user.resolver.ts
+++ technologies/graphql/nest_graphql/src/user/user.resolver.ts
@@ -1,4 +1,27 @@
-import { Resolver } from '@nestjs/graphql';
+import { Args, Int, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { User } from './entities/user.entity';
+import { UserService } from './user.service';
 
-@Resolver()
-export class UserResolver {}
+@Resolver(() => User)
+export class UserResolver {
+  constructor(private readonly userService: UserService) {}
+
+  @Query(() => [User], { name: 'users' })
+  findAll(): User[] {
+    return this.userService.findAll();
+  }
+
+  @Query(() => User, { name: 'user' })
+  findOne(@Args('id', { type: () => Int }) id: number): User {
+    return this.userService.findOne(id);
+  }
+
+  @Mutation(() => User)
+  createUser(
+    @Args('name') name: string,
+    @Args('age', { type: () => Int }) age: number,
+    @Args('email') email: string,
+  ): User {
+    return this.userService.create({ name, age, email });
+  }
+}
