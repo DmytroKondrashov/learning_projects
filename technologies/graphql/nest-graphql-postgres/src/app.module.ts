be555d5222e18c9dfc453f78ce326bdedcf4a17a
DmytroKondrashov <kondrashovd101@gmail.com>
2025-01-22 12:30:56 +0200

    add Apollo driver

--- technologies/graphql/nest-graphql-postgres/src/app.module.ts
+++ technologies/graphql/nest-graphql-postgres/src/app.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver } from '@nestjs/apollo';
 
 @Module({
   imports: [
     GraphQLModule.forRoot({
+      driver: ApolloDriver,
       autoSchemaFile: true,
     }),
   ],
