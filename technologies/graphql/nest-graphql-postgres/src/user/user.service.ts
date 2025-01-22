e75053d1083e1ff617785da3a77b25d79d6713d4
DmytroKondrashov <kondrashovd101@gmail.com>
2025-01-22 12:46:37 +0200

    finish user service

--- technologies/graphql/nest-graphql-postgres/src/user/user.service.ts
+++ technologies/graphql/nest-graphql-postgres/src/user/user.service.ts
@@ -1,4 +1,24 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async findAll() {
+    return this.prisma.user.findMany();
+  }
+
+  async findOnde(id: number) {
+    return this.prisma.user.findUnique({
+      where: { id },
+      include: { posts: true },
+    });
+  }
+
+  async create(email: string) {
+    return this.prisma.user.create({
+      data: { email },
+    });
+  }
+}
