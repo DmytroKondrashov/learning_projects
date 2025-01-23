4146552341802474cbe989310ccb1fa507f3639b
DmytroKondrashov <kondrashovd101@gmail.com>
2025-01-23 11:05:25 +0200

    remove posts

--- technologies/graphql/nest-graphql-postgres/src/user/user.service.ts
+++ technologies/graphql/nest-graphql-postgres/src/user/user.service.ts
@@ -15,7 +15,7 @@ export class UserService {
   async findOne(id: number) {
     return this.prisma.user.findUnique({
       where: { id },
-      include: { posts: true },
+      // include: { posts: true },
     });
   }
 
