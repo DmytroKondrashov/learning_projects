cb9a138dcb5fde01209998e6fa36d6109d6d8a4e
DmytroKondrashov <kondrashovd101@gmail.com>
2025-01-23 11:23:14 +0200

    add proper decorator

--- technologies/graphql/nest-graphql-postgres/src/user/models/user.model.ts
+++ technologies/graphql/nest-graphql-postgres/src/user/models/user.model.ts
@@ -1,6 +1,7 @@
-import { Field } from '@nestjs/graphql';
+import { Field, ObjectType } from '@nestjs/graphql';
 import { Post } from 'src/post/models/post.model';
 
+@ObjectType()
 export class User {
   @Field()
   id: number;
