84e6525d136e22b2daf4106031f86995907f23da
DmytroKondrashov <kondrashovd101@gmail.com>
2025-01-23 10:38:16 +0200

    finish user resolver

--- technologies/graphql/nest-graphql-postgres/src/user/user.resolver.ts
+++ technologies/graphql/nest-graphql-postgres/src/user/user.resolver.ts
@@ -1,4 +1,12 @@
-import { Resolver } from '@nestjs/graphql';
+import { Query, Resolver } from '@nestjs/graphql';
+import { UserService } from './user.service';
+import { User } from './models/user.model';
+@Resolver(() => User)
+export class UserResolver {
+  constructor(private readonly userService: UserService) {}
 
-@Resolver()
-export class UserResolver {}
+  @Query()
+  async users() {
+    return this.userService.findAll();
+  }
+}
