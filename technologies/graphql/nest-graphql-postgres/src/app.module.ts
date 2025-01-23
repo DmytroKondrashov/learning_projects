a00aa2d973344ea07f3fe5045a54b9589c92978b
DmytroKondrashov <kondrashovd101@gmail.com>
2025-01-23 10:18:12 +0200

    add post module, service and resolver

--- technologies/graphql/nest-graphql-postgres/src/app.module.ts
+++ technologies/graphql/nest-graphql-postgres/src/app.module.ts
@@ -5,6 +5,7 @@ import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver } from '@nestjs/apollo';
 import { PrismaService } from './prisma/prisma.service';
 import { UserModule } from './user/user.module';
+import { PostModule } from './post/post.module';
 
 @Module({
   imports: [
@@ -13,6 +14,7 @@ import { UserModule } from './user/user.module';
       autoSchemaFile: true,
     }),
     UserModule,
+    PostModule,
   ],
   controllers: [AppController],
   providers: [AppService, PrismaService],
