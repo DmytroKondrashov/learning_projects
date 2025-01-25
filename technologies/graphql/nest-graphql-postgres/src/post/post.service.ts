cced5b4e9bd1e80494e829f126373d734969e250
DmytroKondrashov <kondrashovd101@gmail.com>
2025-01-25 19:02:26 +0200

    finish posts service

--- technologies/graphql/nest-graphql-postgres/src/post/post.service.ts
+++ technologies/graphql/nest-graphql-postgres/src/post/post.service.ts
@@ -1,4 +1,27 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
-export class PostService {}
+export class PostService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async findAll() {
+    return this.prisma.post.findMany();
+  }
+
+  async findOne(id: number) {
+    return this.prisma.post.findUnique({ where: { id } });
+  }
+
+  async create(title: string, userId: number) {
+    return this.prisma.post.create({ data: { title, userId } });
+  }
+
+  async update(id: number, title: string) {
+    return this.prisma.post.update({ where: { id }, data: { title } });
+  }
+
+  async delete(id: number) {
+    return this.prisma.post.delete({ where: { id } });
+  }
+}
