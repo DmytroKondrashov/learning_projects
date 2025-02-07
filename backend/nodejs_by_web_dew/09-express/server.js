86be9e5476fe5d7c00e391e0c1776bd4f83be031
DmytroKondrashov <kondrashovd101@gmail.com>
2025-02-07 12:38:57 +0200

    add basic aexpress example

---
+++ backend/nodejs_by_web_dew/09-express/server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+const port = 3000;
+
+const createPath = (page) => path.resolve(__dirname, 'views', `${page}.html`);
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
+app.get('/', (req, res) => {
+  res.sendFile(createPath('index'));
+});
+
+app.get('/contacts', (req, res) => {
+  res.sendFile(createPath('contacts'));
+});
+
+app.get('/about-us', (req, res) => {
+  res.redirect('/contacts');
+});
+
+app.use((req, res) => {
+  res.status(404).sendFile(createPath('error'));
+});
