4451ee8ef76458a1250ec81ce3522edf4a22ec63
DmytroKondrashov <kondrashovd101@gmail.com>
2025-02-11 17:52:11 +0200

    remove old apos app

--- fullstack/apostrophe/apos-app/modules/@apostrophecms/asset/index.js
+++
@@ -1,9 +0,0 @@
-export default {
-  // When not in production, refresh the page on restart
-  options: {
-    // Change to `apos` for admin UI HMR
-    // `public` is the default and provides HMR for project UI code
-    hmr: 'public',
-    refreshOnRestart: true
-  }
-};
