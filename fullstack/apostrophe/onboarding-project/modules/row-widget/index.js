2b84b0f8d4ffc191eef99f2cae6ffb5c205b6eb1
DmytroKondrashov <kondrashovd101@gmail.com>
2025-02-25 17:52:26 +0200

    add icon and preview icon

--- fullstack/apostrophe/onboarding-project/modules/row-widget/index.js
+++ fullstack/apostrophe/onboarding-project/modules/row-widget/index.js
@@ -2,6 +2,8 @@ export default {
   extend: '@apostrophecms/widget-type',
   options: {
     label: 'Row Widget',
+    icon: 'view-column-outline',
+    previewIcon: 'view-column-outline'
   },
   fields: {
     add: {}
