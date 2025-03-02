5f1e11e1a8c11ea0f9c35ff6cd421b888c12c819
DmytroKondrashov <kondrashovd101@gmail.com>
2025-03-02 11:13:07 +0200

    add basic rating widget co-nfiguration

--- fullstack/apostrophe/onboarding-project/modules/rating-widget/index.js
+++ fullstack/apostrophe/onboarding-project/modules/rating-widget/index.js
@@ -4,6 +4,49 @@ export default {
     label: 'Rating Widget',
   },
   fields: {
-    add: {}
+    add: {
+      productQuality: {
+        type: 'range',
+        label: 'Product Quality',
+        min: 1,
+        max: 5,
+        step: 1,
+        def: 1
+      },
+      productSafety: {
+        type: 'range',
+        label: 'Product Safety',
+        min: 1,
+        max: 5,
+        step: 1,
+        def: 1
+      },
+      productValue: {
+        type: 'range',
+        label: 'Product Value',
+        min: 1,
+        max: 5,
+        step: 1,
+        def: 1
+      }
+    },
+    group: {
+      basics: {
+        label: 'Basics',
+        fields: [ 'productQuality', 'productSafety', 'productValue' ]
+      }
+    }
+  },
+  components(self) {
+    return {
+      async stars(req, data) {
+        // Performs any data manipulation or retrieval
+        // before it is passed to the component template
+        // In this case, we are just passing the starsData from the widget
+        return {
+          starsData: data.starsData
+        };
+      }
+    };
   }
 };
