98a8ed44e24fea4944da46d3ff9a44fa7edab24a
DmytroKondrashov <kondrashovd101@gmail.com>
2025-03-04 22:16:32 +0200

    add flowers resource

--- backend/nestjs/flowers-store/src/app.module.ts
+++ backend/nestjs/flowers-store/src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { App1Controller } from './app-1/app-1.controller';
+import { FlowersModule } from './flowers/flowers.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [FlowersModule],
+  controllers: [AppController, App1Controller],
   providers: [AppService],
 })
 export class AppModule {}
