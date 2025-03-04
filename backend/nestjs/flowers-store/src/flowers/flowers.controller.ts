dcef0cf0f77ec03b1b51a9b82a36d6fa1936a007
DmytroKondrashov <kondrashovd101@gmail.com>
2025-03-04 22:16:50 +0200

    prettier fix

--- backend/nestjs/flowers-store/src/flowers/flowers.controller.ts
+++ backend/nestjs/flowers-store/src/flowers/flowers.controller.ts
@@ -1,4 +1,12 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { FlowersService } from './flowers.service';
 import { CreateFlowerDto } from './dto/create-flower.dto';
 import { UpdateFlowerDto } from './dto/update-flower.dto';
