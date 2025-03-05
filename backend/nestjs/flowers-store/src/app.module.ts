286bbce6fe3bd409c7b08fbee656770475365cfe
DmytroKondrashov <kondrashovd101@gmail.com>
2025-03-05 11:25:31 +0200

    remove unneeded import

--- backend/nestjs/flowers-store/src/app.module.ts
+++ backend/nestjs/flowers-store/src/app.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { App1Controller } from './app-1/app-1.controller';
 import { FlowersModule } from './flowers/flowers.module';
 
 @Module({
   imports: [FlowersModule],
-  controllers: [AppController, App1Controller],
+  controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
