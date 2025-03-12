4bd6ec887a18166cbd779ac67d81d2e8c3a3d17d
DmytroKondrashov <kondrashovd101@gmail.com>
2025-03-12 20:26:44 +0200

    use correct type

--- backend/nestjs/flowers-store/src/flowers/flowers.controller.ts
+++ backend/nestjs/flowers-store/src/flowers/flowers.controller.ts
@@ -11,18 +11,18 @@ import {
   UseInterceptors,
 } from '@nestjs/common';
 import { FlowersService } from './flowers.service';
-import { CreateFlowerDto } from './dto/create-flower.dto';
 import { UpdateFlowerDto } from './dto/update-flower.dto';
 import { ParseIntPipe } from 'src/pipes/pipe';
 import { AuthGuard } from 'src/guards/guard';
 import { LoggingInterceptor } from 'src/interceptors/interceptor';
+import { FlowersCreateDto } from 'src/dtos/flowers.dto';
 
 @Controller('flowers')
 export class FlowersController {
   constructor(private readonly flowersService: FlowersService) {}
 
   @Post()
-  create(@Body() createFlowerDto: CreateFlowerDto) {
+  create(@Body() createFlowerDto: FlowersCreateDto) {
     return this.flowersService.create(createFlowerDto);
   }
 
