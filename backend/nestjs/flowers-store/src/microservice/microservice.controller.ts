adc8db70dbb4cd9b29da88c95f6531ec1b5402c0
DmytroKondrashov <kondrashovd101@gmail.com>
2025-03-14 22:07:14 +0200

    add message handler

--- backend/nestjs/flowers-store/src/microservice/microservice.controller.ts
+++ backend/nestjs/flowers-store/src/microservice/microservice.controller.ts
@@ -1,7 +1,13 @@
 import { Controller } from '@nestjs/common';
 import { MicroserviceService } from './microservice.service';
+import { EventPattern } from '@nestjs/microservices';
 
 @Controller()
 export class MicroserviceController {
   constructor(private readonly microserviceService: MicroserviceService) {}
+
+  @EventPattern('flowers-store')
+  handleMessage(message) {
+    this.microserviceService.handleMessage(message);
+  }
 }
