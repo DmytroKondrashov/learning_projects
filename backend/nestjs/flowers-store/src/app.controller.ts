b4774cef88fe41a962ae5349ec3ca6ab9317fa54
DmytroKondrashov <kondrashovd101@gmail.com>
2025-03-14 22:13:53 +0200

    finish newOrder method

--- backend/nestjs/flowers-store/src/app.controller.ts
+++ backend/nestjs/flowers-store/src/app.controller.ts
@@ -5,8 +5,9 @@ import { AppService } from './app.service';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get('new-order')
+  newOrder() {
+    this.appService.sendMessage();
+    return 'Message sent';
   }
 }
