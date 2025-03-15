8c0b0d398f43578e709b67f5d422e5d12da450c5
DmytroKondrashov <kondrashovd101@gmail.com>
2025-03-15 10:34:59 +0200

    return FlowerModel array

--- backend/nestjs/flowers-store/src/flowers-gql/flowers-gql.resolver.ts
+++ backend/nestjs/flowers-store/src/flowers-gql/flowers-gql.resolver.ts
@@ -1,11 +1,12 @@
 import { Query, Resolver } from '@nestjs/graphql';
 import { FlowersService } from 'src/flowers/flowers.service';
+import { FlowerModel } from './models/flower.model';
 
 @Resolver()
 export class FlowersGqlResolver {
   constructor(private readonly flowersService: FlowersService) {}
 
-  @Query(() => String, { name: 'flowers' })
+  @Query(() => [FlowerModel], { name: 'flowers' })
   findAll() {
     return this.flowersService.findAll();
   }
