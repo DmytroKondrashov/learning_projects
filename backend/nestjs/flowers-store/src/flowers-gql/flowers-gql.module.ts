c52f28535d2ba973eed2b4d9c3ecc320048961fe
DmytroKondrashov <kondrashovd101@gmail.com>
2025-03-15 10:18:31 +0200

    fix imports

--- backend/nestjs/flowers-store/src/flowers-gql/flowers-gql.module.ts
+++ backend/nestjs/flowers-store/src/flowers-gql/flowers-gql.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { FlowersGqlResolver } from './flowers-gql.resolver';
 import { FlowersService } from 'src/flowers/flowers.service';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Module({
-  providers: [FlowersGqlResolver, FlowersService],
+  providers: [FlowersGqlResolver, FlowersService, PrismaService],
 })
 export class FlowersGqlModule {}
