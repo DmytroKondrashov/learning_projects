9827b0c45b9e9a0c69fefbc8abce9b5628363978
DmytroKondrashov <kondrashovd101@gmail.com>
2025-03-16 11:30:57 +0200

    add service specs

---
+++ backend/nestjs/flowers-store/src/flowers/flower.service.spec.ts
@@ -0,0 +1,62 @@
+import { Test } from '@nestjs/testing';
+import { FlowersService } from './flowers.service';
+
+describe('FlowerService', () => {
+  let service: FlowersService;
+
+  beforeEach(async () => {
+    const module = await Test.createTestingModule({
+      providers: [
+        FlowersService,
+        {
+          provide: 'PrismaService',
+          useValue: {
+            flower: {
+              findMany: jest.fn().mockResolvedValue([
+                {
+                  id: 1,
+                  name: 'Rose',
+                  color: 'Red',
+                  price: 10,
+                },
+              ]),
+              create: jest.fn().mockResolvedValue({
+                id: 1,
+                name: 'Rose',
+                color: 'Red',
+                price: 10,
+              }),
+            },
+          },
+        },
+      ],
+    }).compile();
+
+    service = module.get<FlowersService>(FlowersService);
+  });
+
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+  });
+
+  it('should return an array of flowers', async () => {
+    expect(await service.findAll()).toEqual([
+      {
+        id: 1,
+        name: 'Rose',
+        color: 'Red',
+        price: 10,
+      },
+    ]);
+  });
+
+  it('should create a flower', async () => {
+    expect(
+      await service.create({
+        name: 'Rose',
+        color: 'Red',
+        price: 10,
+      }),
+    ).toEqual({ id: 1, name: 'Rose', color: 'Red', price: 10 });
+  });
+});
