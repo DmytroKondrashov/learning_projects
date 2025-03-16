6f597b8e23ad70510cc3cc978fc94a6b4d085058
DmytroKondrashov <kondrashovd101@gmail.com>
2025-03-16 11:52:02 +0200

    add e2e tests

---
+++ backend/nestjs/flowers-store/src/flowers/flowers.e2e-spec.ts
@@ -0,0 +1,51 @@
+import { INestApplication, ValidationPipe } from '@nestjs/common';
+import { Test, TestingModule } from '@nestjs/testing';
+import * as request from 'supertest';
+import { AppModule } from 'src/app.module';
+
+describe('FlowersController (e2e)', () => {
+  let app: INestApplication;
+
+  beforeAll(async () => {
+    const moduleMixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    app = moduleMixture.createNestApplication();
+    app.useGlobalPipes(new ValidationPipe());
+    await app.init();
+  });
+
+  it('/flowers (GET)', async () => {
+    return request(app.getHttpServer())
+      .get('/flowers')
+      .expect(200)
+      .expect([
+        {
+          id: 1,
+          name: 'Rose',
+          color: 'Red',
+          price: 10,
+        },
+      ]);
+  });
+
+  it('/flowers (POST)', async () => {
+    return request(app.getHttpServer())
+      .post('/flowers')
+      .send({ name: 'Rose', color: 'Red', price: 10 })
+      .expect(201)
+      .expect({
+        id: 1,
+        name: 'Rose',
+        color: 'Red',
+        price: 10,
+        createdAt: expect.any(String),
+        updatedAt: expect.any(String),
+      });
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+});
