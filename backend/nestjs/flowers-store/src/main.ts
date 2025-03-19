7219ce25e1b741888b62ca89bc11cce0305e7ee3
DmytroKondrashov <kondrashovd101@gmail.com>
2025-03-19 12:06:46 +0200

    add Swagger config

--- backend/nestjs/flowers-store/src/main.ts
+++ backend/nestjs/flowers-store/src/main.ts
@@ -2,9 +2,20 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import 'reflect-metadata';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Flowers Store')
+    .setDescription('The flowers store API description')
+    .setVersion('1.0')
+    .addTag('flowers')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
   await app.listen(3000);
 
   const microserviceApp =
