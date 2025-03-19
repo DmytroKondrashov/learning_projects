e89b480a11826e4378f50ac79bad9c9139cb25e3
DmytroKondrashov <kondrashovd101@gmail.com>
2025-03-19 12:15:44 +0200

    add swagger to dto

--- backend/nestjs/flowers-store/src/dtos/flowers.dto.ts
+++ backend/nestjs/flowers-store/src/dtos/flowers.dto.ts
@@ -1,15 +1,29 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsString, IsNotEmpty, IsNumber } from 'class-validator';
 
 export class FlowersCreateDto {
   @IsString()
   @IsNotEmpty()
+  @ApiProperty({
+    description: 'The name of the flower',
+    example: 'Rose',
+  })
   name: string;
 
   @IsString()
   @IsNotEmpty()
+  @ApiProperty({
+    description: 'The color of the flower',
+    example: 'Red',
+  })
   color: string;
 
   @IsNumber()
+  @IsNotEmpty()
+  @ApiProperty({
+    description: 'The price of the flower',
+    example: 10,
+  })
   price: number;
 }
 
