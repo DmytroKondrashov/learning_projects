d931e83670355e637d6e21b4420706615d81faa8
DmytroKondrashov <kondrashovd101@gmail.com>
2025-03-19 11:38:11 +0200

    fix imports

--- backend/nestjs/flowers-store/src/flowers/flowers.service.ts
+++ backend/nestjs/flowers-store/src/flowers/flowers.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { Flower } from '@prisma/client';
-import { PrismaService } from 'src/prisma/prisma.service';
+import { PrismaService } from '../prisma/prisma.service';
 import { FlowersCreateDto, TFlowersUpdateDto } from 'src/dtos/flowers.dto';
 import { ConfigService } from '@nestjs/config';
 
