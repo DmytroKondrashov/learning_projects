9f1e30ecc754edbbb00bad0c8fe69db1e8e97fd4
DmytroKondrashov <kondrashovd101@gmail.com>
2025-04-18 12:11:51 +0300

    fix db creation script

--- small_projects/flashcard/backend/setup_db.sh
+++ small_projects/flashcard/backend/setup_db.sh
@@ -5,7 +5,7 @@ source .env
 
 # Create the database
 echo "Creating database: $DB_NAME"
-psql -U "$DB_USER" -h "$DB_HOST" -p "$DB_PORT" -c "CREATE DATABASE $DB_NAME;"
+psql -U "$DB_USER" -d postgres -h "$DB_HOST" -p "$DB_PORT" -c "CREATE DATABASE $DB_NAME;"
 
 # Create the table and seed data
 echo "Setting up schema and inserting initial data..."
