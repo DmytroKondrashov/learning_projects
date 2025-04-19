96edaff7bb09b9d12f6bcb3517f6716416339521
DmytroKondrashov <kondrashovd101@gmail.com>
2025-04-19 11:17:33 +0300

    send real data to parser

--- small_projects/flashcard/backend/scripts/csv_parser.js
+++ small_projects/flashcard/backend/scripts/csv_parser.js
@@ -1,11 +1,18 @@
 const assert = require("assert");
 const { parse } = require("csv-parse");
+const fs = require("fs");
+const path = require("path");
 
 const records = [];
 // Initialize the parser
 const parser = parse({
   delimiter: ",",
 });
+
+// Read the CSV file and pipe it to the parser
+const csvFilePath = path.join(__dirname, "../common_words.csv");
+fs.createReadStream(csvFilePath).pipe(parser);
+
 // Use the readable stream api to consume records
 parser.on("readable", function () {
   let record;
@@ -13,13 +20,13 @@ parser.on("readable", function () {
     records.push(record);
   }
 });
+
 // Catch any error
 parser.on("error", function (err) {
   console.error(err.message);
 });
+
 // Test that the parsed records matched the expected records
 parser.on("end", function () {
   console.log(records);
 });
-// Close the readable stream
-parser.end();
