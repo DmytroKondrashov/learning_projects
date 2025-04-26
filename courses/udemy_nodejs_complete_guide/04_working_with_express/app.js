01bfa79c642805a8e95e6131f7cb2c250d80b5f7
DmytroKondrashov <kondrashovd101@gmail.com>
2025-04-26 11:02:43 +0300

    use admin router

--- courses/udemy_nodejs_complete_guide/04_working_with_express/app.js
+++ courses/udemy_nodejs_complete_guide/04_working_with_express/app.js
@@ -1,9 +1,11 @@
 const express  = require('express');
 const bodyParser = require('body-parser');
-
+const adminRoutes = require('./routes/admin');
 const app = express();
 
 app.use(bodyParser.urlencoded({extended: false}));
+
+app.use('/admin', adminRoutes);
 
 app.use('/', (req, res, next) => {
   console.log('In the another middleware!');
