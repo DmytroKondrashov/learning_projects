fe0d32d5badfb4e95b11ec4ebba7dd8a8fb97aa3
DmytroKondrashov <kondrashovd101@gmail.com>
2025-04-27 10:07:34 +0300

    use shop router

--- courses/udemy_nodejs_complete_guide/04_working_with_express/app.js
+++ courses/udemy_nodejs_complete_guide/04_working_with_express/app.js
@@ -1,15 +1,13 @@
 const express  = require('express');
 const bodyParser = require('body-parser');
 const adminRoutes = require('./routes/admin');
+const shopRouter = require('./routes/shop');
 const app = express();
 
 app.use(bodyParser.urlencoded({extended: false}));
 
 app.use('/admin', adminRoutes);
 
-app.use('/', (req, res, next) => {
-  console.log('In the another middleware!');
-  res.send('<h1>Hello from Express!</h1>');
-});
+app.use(shopRouter);
 
 app.listen(3000);
