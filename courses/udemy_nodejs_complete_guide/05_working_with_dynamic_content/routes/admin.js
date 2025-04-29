3b5784891fe43d8d5256e4e1041c22bbfeffb680
DmytroKondrashov <kondrashovd101@gmail.com>
2025-04-29 13:42:02 +0300

    use template for add product functionality

--- courses/udemy_nodejs_complete_guide/05_working_with_dynamic_content/routes/admin.js
+++ courses/udemy_nodejs_complete_guide/05_working_with_dynamic_content/routes/admin.js
@@ -7,8 +7,8 @@ const router = express.Router();
 const products = [];
 
 router.get('/add-product', (req, res, next) => {
-  console.log('In the another middleware!');
-  res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
+  // res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
+  res.render('add-product', {docTitle: 'Add Product'});
 });
 
 router.post('/add-product', (req, res, next) => {
