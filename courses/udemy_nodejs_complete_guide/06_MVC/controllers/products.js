b1fc3591d2c3a5c25517e3c0fa20f83ce04894d0
DmytroKondrashov <kondrashovd101@gmail.com>
2025-05-03 10:04:47 +0300

    move post logic to controller

--- courses/udemy_nodejs_complete_guide/06_MVC/controllers/products.js
+++ courses/udemy_nodejs_complete_guide/06_MVC/controllers/products.js
@@ -1,4 +1,11 @@
+const products = [];
+
 exports.getAddProduct =(req, res, next) => {
   // res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
   res.render('add-product', {pageTitle: 'Add Product', path: '/admin/add-product', formsCSS: true, productCSS: true, activeAddProduct: true});
+}
+
+exports.postAddProduct = (req, res, next) => {
+  console.log(req.body);
+  res.redirect('/');
 }
