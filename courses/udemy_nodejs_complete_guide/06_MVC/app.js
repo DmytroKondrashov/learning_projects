fdfeab81b7f3a9c3fd192a637766cfa7e3f0c6bc
DmytroKondrashov <kondrashovd101@gmail.com>
2025-05-03 10:11:56 +0300

    fix naming

--- courses/udemy_nodejs_complete_guide/06_MVC/app.js
+++ courses/udemy_nodejs_complete_guide/06_MVC/app.js
@@ -1,6 +1,6 @@
 const express  = require('express');
 const bodyParser = require('body-parser');
-const adminData = require('./routes/admin');
+const adminRoutes = require('./routes/admin');
 const shopRouter = require('./routes/shop');
 const path = require('path');
 // const expressHbs = require('express-handlebars');
@@ -16,7 +16,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin', adminData.routes);
+app.use('/admin', adminRoutes);
 
 app.use(shopRouter);
 
