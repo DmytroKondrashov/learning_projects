9bf8b4c41b5658b9ad688c059edbf25ef81bc748
DmytroKondrashov <kondrashovd101@gmail.com>
2025-05-03 10:02:54 +0300

    move middleware to controler

---
+++ courses/udemy_nodejs_complete_guide/06_MVC/controllers/products.js
@@ -0,0 +1,4 @@
+exports.getAddProduct =(req, res, next) => {
+  // res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
+  res.render('add-product', {pageTitle: 'Add Product', path: '/admin/add-product', formsCSS: true, productCSS: true, activeAddProduct: true});
+}
