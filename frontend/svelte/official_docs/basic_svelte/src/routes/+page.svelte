48ee091085ca65cb65059060baa076a55632568a
DmytroKondrashov <kondrashovd101@gmail.com>
2025-05-06 11:00:04 +0300

    add dynamic attributes

--- frontend/svelte/official_docs/basic_svelte/src/routes/+page.svelte
+++ frontend/svelte/official_docs/basic_svelte/src/routes/+page.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
   let name = "Svelte"
+  let src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQzTojBf4VJCGR4Z-QxG-7GozKDuWjCst6z6Q&s"
 </script>
 
 <h1>Hello {name.toUpperCase()}</h1>
+<img src={src} alt="A cat">
