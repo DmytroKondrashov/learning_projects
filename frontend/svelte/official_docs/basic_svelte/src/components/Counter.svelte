5af6085dd190c6d42aed0f6a375fa5a55e9e859f
DmytroKondrashov <kondrashovd101@gmail.com>
2025-05-06 13:01:51 +0300

    fix import

--- frontend/svelte/official_docs/basic_svelte/src/components/Counter.svelte
+++ frontend/svelte/official_docs/basic_svelte/src/components/Counter.svelte
@@ -1,5 +1,5 @@
 <script>
-  import { counter } from './shared.js';
+  import { counter } from '../shared/shared.js';
 </script>
 
 <button onclick={() => counter.count +=1}> clicks: {counter.count}</button>
