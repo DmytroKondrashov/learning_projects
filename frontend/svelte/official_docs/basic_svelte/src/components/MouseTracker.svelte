37d0c5698bc48555504c9fa02ac6228f6db21253
DmytroKondrashov <kondrashovd101@gmail.com>
2025-05-06 17:42:55 +0300

    create Mouse Tracker component

---
+++ frontend/svelte/official_docs/basic_svelte/src/components/MouseTracker.svelte
@@ -0,0 +1,26 @@
+<script>
+  let m = $state({ x: 0, y: 0 })
+
+  /**
+   * @param {{ clientX: number; clientY: number; }} event
+   */
+  function onpointermove(event) {
+    m.x = event.clientX;
+    m.y = event.clientY;
+  }
+</script>
+
+<div>
+  The pointer is at {Math.round(m.x)} x {Math.round(m.y)}
+</div>
+
+<style>
+	div {
+		position: fixed;
+		left: 0;
+		top: 0;
+		width: 100%;
+		height: 100%;
+		padding: 1rem;
+	}
+</style>
