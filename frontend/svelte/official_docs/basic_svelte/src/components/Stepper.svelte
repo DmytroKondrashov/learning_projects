85704433d89366b753fa3bd2a21b94c73ecb86ed
DmytroKondrashov <kondrashovd101@gmail.com>
2025-05-06 19:07:37 +0300

    use params in Stepper

--- frontend/svelte/official_docs/basic_svelte/src/components/Stepper.svelte
+++ frontend/svelte/official_docs/basic_svelte/src/components/Stepper.svelte
@@ -1,6 +1,6 @@
 <script>
-  let {} = $props();
+  let { increment, decrement } = $props();
 </script>
 
-<button>+1</button>
-<button>-1</button>
+<button onclick={increment}>+1</button>
+<button onclick={decrement}>-1</button>
