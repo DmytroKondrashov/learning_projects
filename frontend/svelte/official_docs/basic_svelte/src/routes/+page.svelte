e77afb4aa8a967def3a8ca7895f092b45bf82dfa
DmytroKondrashov <kondrashovd101@gmail.com>
2025-05-06 11:17:11 +0300

    raw HTML usage

--- frontend/svelte/official_docs/basic_svelte/src/routes/+page.svelte
+++ frontend/svelte/official_docs/basic_svelte/src/routes/+page.svelte
@@ -3,13 +3,14 @@
 
   let name = "Svelte"
   let src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQzTojBf4VJCGR4Z-QxG-7GozKDuWjCst6z6Q&s"
+  let rawHTML = `this string contains some <strong>HTML</strong>`
 </script>
 
 <h1>Hello {name.toUpperCase()}</h1>
 <img {src} alt="A cat">
 <p>This is a paragraph</p>
 <Nested />
-
+<div>{@html rawHTML}</div>
 <style>
   p {
     color: goldenrod;
