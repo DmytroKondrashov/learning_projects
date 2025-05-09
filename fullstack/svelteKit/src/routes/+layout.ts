90d978a84049237e41eb3bddc5bf190c1f2a93b6
DmytroKondrashov <kondrashovd101@gmail.com>
2025-05-09 15:56:39 +0300

    use pokemon url

--- fullstack/svelteKit/src/routes/+layout.ts
+++ fullstack/svelteKit/src/routes/+layout.ts
@@ -2,7 +2,7 @@ import { Client, cacheExchange, fetchExchange } from '@urql/svelte';
 
 export default function load() {
   const client = new Client({
-    url: 'http://localhost:3000/graphql',
+    url: 'https://beta.pokeapi.co/graphql/v1beta',
     exchanges: [cacheExchange, fetchExchange],
   });
 
