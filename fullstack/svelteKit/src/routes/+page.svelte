7e6e03c653a3e77a23f5cd567c309b9b887f684b
DmytroKondrashov <kondrashovd101@gmail.com>
2025-05-09 16:14:11 +0300

    add query

--- fullstack/svelteKit/src/routes/+page.svelte
+++ fullstack/svelteKit/src/routes/+page.svelte
@@ -1,2 +1,31 @@
-<h1 class="has-text-link">Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+  import { gql } from '@urql/svelte';
+
+  let { data } = $props();
+
+  const query = gql`
+      query Dragons {
+        dragons {
+          name
+          first_flight
+          diameter {
+            feet
+          }
+          launch_payload_mass {
+            lb
+          }
+        }
+}
+    `
+</script>
+
+{#await data.client.query(query, { }).toPromise()}
+  <div>Loading...</div>
+{:then { data, error }}
+  {#if error }
+    <div>Error: {error.message}</div>
+  {/if}
+  <pre>{JSON.stringify(data, null, 2)}</pre>
+{:catch error}
+  <div>Error: {error.message}</div>
+{/await}
