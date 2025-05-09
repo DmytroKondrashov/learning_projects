2da02b280b9c929bdc1ba5285b005c69dc4f1b1a
DmytroKondrashov <kondrashovd101@gmail.com>
2025-05-09 16:35:02 +0300

    use updated query

--- fullstack/svelteKit/src/routes/+page.svelte
+++ fullstack/svelteKit/src/routes/+page.svelte
@@ -4,22 +4,31 @@
   let { data } = $props();
 
   const query = gql`
-      query Dragons {
-        dragons {
-          name
-          first_flight
-          diameter {
-            feet
-          }
-          launch_payload_mass {
-            lb
-          }
-        }
+      query getPosts($first: Int) {
+  posts(first: $first) {
+    id
+    title
+    body
+    comments(first: $first) {
+      id
+      name
+      email
+      body
+    }
+    user {
+      id
+      name
+      username
+      email
+      phone
+      website
+    }
+  }
 }
     `
 </script>
 
-{#await data.client.query(query, { }).toPromise()}
+{#await data.client.query(query, { first: 10}).toPromise()}
   <div>Loading...</div>
 {:then { data, error }}
   {#if error }
