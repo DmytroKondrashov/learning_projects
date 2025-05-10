b93b0f23df862b3cfa8d02c8ff113b1a444e2dc2
DmytroKondrashov <kondrashovd101@gmail.com>
2025-05-10 11:07:11 +0300

    add 404 handler

--- courses/svelte/official_docs/basic_svelte_kit/src/routes/blog/[slug]/+page.server.js
+++ courses/svelte/official_docs/basic_svelte_kit/src/routes/blog/[slug]/+page.server.js
@@ -1,7 +1,10 @@
 import { posts } from '../data'
+import { error } from '@sveltejs/kit'
 
 export function load({ params }) {
   const post = posts.find((post) => post.slug === params.slug);
+
+  if (!post) error(404);
 
   return { post }
 }
