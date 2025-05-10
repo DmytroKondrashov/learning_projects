87a4e94e01929baf086e4f77d775054407a3e700
DmytroKondrashov <kondrashovd101@gmail.com>
2025-05-10 12:55:53 +0300

    get todos by userId

--- courses/svelte/official_docs/basic_svelte_kit/src/routes/+page.server.js
+++ courses/svelte/official_docs/basic_svelte_kit/src/routes/+page.server.js
@@ -6,12 +6,27 @@
 //   }) 
 // }
 
+// export function load({ cookies }) {
+//   const visited = cookies.get('visited');
+
+//   cookies.set('visited', 'true', {path: '/'});
+
+//   return {
+//     visited: visited === 'true'
+//   }
+// }
+
+import * as db from '$lib/server/database';
+
 export function load({ cookies }) {
-  const visited = cookies.get('visited');
+  let id = cookies.get('userId');
 
-  cookies.set('visited', 'true', {path: '/'});
+  if(!id) {
+    id = crypto.randomUUID();
+    cookies.set('userId', id, {path: '/'});
 
-  return {
-    visited: visited === 'true'
+    return {
+      todos: db.getTodos(id)
+    }
   }
 }
