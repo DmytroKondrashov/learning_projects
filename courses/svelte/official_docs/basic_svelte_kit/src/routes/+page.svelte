ba082296e45ccc1ddebd05868af5bb416b6c53d6
DmytroKondrashov <kondrashovd101@gmail.com>
2025-05-12 22:01:09 +0300

    delete todos immidiatly

--- courses/svelte/official_docs/basic_svelte_kit/src/routes/+page.svelte
+++ courses/svelte/official_docs/basic_svelte_kit/src/routes/+page.svelte
@@ -40,9 +40,19 @@
 </form>
 
 	<ul class="todos">
-		{#each data.todos as todo (todo.id)}
+		{#each data.todos.filter((todo) => !deleting.includes(todo.id)) as todo (todo.id)}
 			<li in:fly={ {y: 20} } out:slide>
-				<form action="?/delete" method="POST" use:enhance>
+				<form 
+					action="?/delete" 
+					method="POST" 
+					use:enhance={() => {
+						deleting = [...deleting, todo.id];
+						return async ({ update }) => {
+							await update()
+							deleting = deleting.filter((id) => id != todo.id)
+						}
+					}}
+				>
           <input type="hidden" name='id' value={todo.id}>
           <span>{todo.description}</span>
           <button type="submit" aria-label="Mark as complete">X</button>
