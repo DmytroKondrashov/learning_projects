da92d3648fc70da688de0ba213980531969174ff
DmytroKondrashov <kondrashovd101@gmail.com>
2025-05-12 22:18:13 +0300

    create a roll page

---
+++ courses/svelte/official_docs/basic_svelte_kit/src/routes/roll/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+  let number = $state()
+
+  async function roll() {
+    const response = await fetch('/roll')
+    number = await response.json();
+  }
+</script>
+
+<button onclick={roll}>Roll the dice!</button>
+
+{#if number !== undefined}
+  <p>You rolled a {number}!</p>
+{/if}
