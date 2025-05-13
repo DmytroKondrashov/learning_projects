208ecd5bf2839b4cd69b332e7fbd6693fbdc135b
DmytroKondrashov <kondrashovd101@gmail.com>
2025-05-13 12:15:25 +0300

    use json shorthand

--- courses/svelte/official_docs/basic_svelte_kit/src/routes/roll/+server.js
+++ courses/svelte/official_docs/basic_svelte_kit/src/routes/roll/+server.js
@@ -1,9 +1,15 @@
+import { json } from '@sveltejs/kit';
+
 export function GET() {
   const number = Math.floor(Math.random() * 6) + 1;
 
-  return new Response(number, {
-    headers: {
-      'Content-Type': 'application/json',
-    }
-  });
+  // Thos is the same as the convenient variant below
+  // return new Response(number, {
+  //   headers: {
+  //     'Content-Type': 'application/json',
+  //   }
+  // });
+
+  // this is the same as the inconvenient variant above
+  return json(number);
 }
