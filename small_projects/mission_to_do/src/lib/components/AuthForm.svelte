4c783f9dc2ac87fe66ad7a99f2142d453fd73bae
DmytroKondrashov <kondrashovd101@gmail.com>
2025-05-15 20:47:51 +0300

    add AuthForm component

---
+++ small_projects/mission_to_do/src/lib/components/AuthForm.svelte
@@ -0,0 +1,33 @@
+<script>
+  export let email = '';
+  export let password = '';
+  export let error = '';
+</script>
+
+<form method="POST" class="auth-form">
+  {#if error}
+    <p class="text-red-500">{error}</p>
+  {/if}
+
+  <label>
+    Email:
+    <input type="email" name="email" bind:value={email} required />
+  </label>
+
+  <label>
+    Password:
+    <input type="password" name="password" bind:value={password} required />
+  </label>
+
+  <button type="submit">Login</button>
+</form>
+
+<style>
+  form {
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+    max-width: 400px;
+    margin: auto;
+  }
+</style>
