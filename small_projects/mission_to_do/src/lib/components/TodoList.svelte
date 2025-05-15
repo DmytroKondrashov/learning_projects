c22a6b8a546b6f45241c3df9b3d8e402cf4031fb
DmytroKondrashov <kondrashovd101@gmail.com>
2025-05-15 19:46:50 +0300

    fill the list component

---
+++ small_projects/mission_to_do/src/lib/components/TodoList.svelte
@@ -0,0 +1,14 @@
+<script>
+  export let list;
+</script>
+
+<h2>{list.name}</h2>
+<ul>
+  {#each list.todo_items as item}
+    <li>
+      <input type="checkbox" bind:checked={item.done} />
+      <span>{item.name}</span>
+      {#if item.due_date}<small>{item.due_date}</small>{/if}
+    </li>
+  {/each}
+</ul>
