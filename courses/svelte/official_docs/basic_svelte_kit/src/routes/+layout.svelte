01bc17c9ce4a728352edaa92bc298290f9a8a2e9
DmytroKondrashov <kondrashovd101@gmail.com>
2025-05-15 12:09:02 +0300

    implement the redirect

--- courses/svelte/official_docs/basic_svelte_kit/src/routes/+layout.svelte
+++ courses/svelte/official_docs/basic_svelte_kit/src/routes/+layout.svelte
@@ -9,6 +9,7 @@
 	<a href="/blog" aria-current={page.url.pathname === '/blog'}>blog</a>
 	<a href="/errors/expected">expected error</a>
 	<a href="/errors/unexpected">unexpected error</a>
+	<a href="/redirect">redirect</a>
 	{#if navigating.to}
 		navigating to {navigating.to.url.pathname}
 	{/if}
