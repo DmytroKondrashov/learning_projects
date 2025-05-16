3e7d581fbb9d4ab8fb3418c84ff2654245301fd3
DmytroKondrashov <kondrashovd101@gmail.com>
2025-05-16 14:18:07 +0300

    create logout page

---
+++ small_projects/mission_to_do/src/routes/logout/+page.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  import { supabase } from '$lib/supabaseClient';
+
+  const handleLogout = async () => {
+    const { error } = await supabase.auth.signOut();
+    if (error) {
+      alert(error.message);
+    } else {
+      alert('Logged out successfully.');
+    }
+  };
+</script>
+
+<button on:click={handleLogout}>Log Out</button>
