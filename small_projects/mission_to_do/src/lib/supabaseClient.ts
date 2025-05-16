b5d00506e8f3329fa4978108405b9006a45f93f4
DmytroKondrashov <kondrashovd101@gmail.com>
2025-05-16 14:52:25 +0300

    delete all the old code

--- small_projects/mission_to_do/src/lib/supabaseClient.ts
+++
@@ -1,8 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-// const supabaseUrl = import.meta.env.PUBLIC_SUPABASE_URL;
-// const supabaseAnonKey = import.meta.env.PUBLIC_SUPABASE_ANON_KEY;
-
-import { PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY } from '$env/static/public';
-
-export const supabase = createClient(PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY);
