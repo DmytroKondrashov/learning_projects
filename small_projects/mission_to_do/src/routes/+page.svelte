bf1394f2eb01867dab8eb61543c84114b29ac5b9
DmytroKondrashov <kondrashovd101@gmail.com>
2025-05-16 14:52:38 +0300

    create the main page

--- small_projects/mission_to_do/src/routes/+page.svelte
+++ small_projects/mission_to_do/src/routes/+page.svelte
@@ -1,2 +1,36 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+  import { onMount } from "svelte";
+  import { supabase } from "$lib/db";
+  import Auth from "$lib/Auth.svelte";
+  import type { User } from "@supabase/supabase-js";
+  import Home from "$lib/Home.svelte";
+
+  let user: User;
+
+  onMount(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      user = session?.user ?? null;
+    });
+
+    const { data: { subscription: authListener } } = supabase.auth.onAuthStateChange(
+      (_, session) => {
+        const currentUser = session?.user;
+        user = currentUser ?? null;
+      }
+    );
+
+    return () => {
+      authListener?.unsubscribe();
+    };
+  });
+</script>
+
+<div class="w-full h-full bg-200">
+  {#if user}
+    <Home bind:user />
+  {:else}
+    <div class="min-w-full min-h-screen flex items-center justify-center">
+      <Auth />
+    </div>
+  {/if}
+</div>
