e05b42426a57cce963b1f81a061b1f2578cf1781
DmytroKondrashov <kondrashovd101@gmail.com>
2025-05-27 22:21:08 +0300

    add db init script

---
+++ small_projects/small_to_do/docker-entrypoint-initdb.d/01_init.sql
@@ -0,0 +1,17 @@
+CREATE ROLE supabase_admin WITH SUPERUSER LOGIN PASSWORD 'postgres';
+
+CREATE EXTENSION IF NOT EXISTS "pg_graphql";
+
+CREATE TABLE todo_list (
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL
+);
+
+CREATE TABLE list_item (
+    id SERIAL PRIMARY KEY,
+    todo_list_id INTEGER NOT NULL REFERENCES todo_list(id) ON DELETE CASCADE,
+    name TEXT NOT NULL,
+    description TEXT,
+    done BOOLEAN DEFAULT FALSE,
+    due_date DATE
+);
