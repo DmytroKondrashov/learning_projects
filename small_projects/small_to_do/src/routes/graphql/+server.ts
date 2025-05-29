6c312296d5a81d06f636236842c2454d6f0b41d3
DmytroKondrashov <kondrashovd101@gmail.com>
2025-05-29 21:01:42 +0300

    connect to pg

--- small_projects/small_to_do/src/routes/graphql/+server.ts
+++ small_projects/small_to_do/src/routes/graphql/+server.ts
@@ -1,10 +1,11 @@
-import { client } from '$lib/postgres';
 import { json } from '@sveltejs/kit';
+import { client } from '$lib/postgres';
+import type { RequestHandler } from '@sveltejs/kit';
 
-export async function POST({ request }) {
-  const { query, variables } = await request.json();
+export const POST: RequestHandler = async ({ request }) => {
+  const { query, variables = {}, operationName = null } = await request.json();
 
-  const result = await client`SELECT graphql.resolve(${query}, ${variables || {}})`;
+  const rows = await client`SELECT graphql.resolve(${query}, ${variables}, ${operationName}) AS result;`;
 
-  return json(result);
-}
+  return json(rows[0].result);
+};
