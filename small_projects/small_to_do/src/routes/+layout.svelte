fd8df4074f7fd4fc8c36410db229ae2bb8405ea2
DmytroKondrashov <kondrashovd101@gmail.com>
2025-05-31 17:45:14 +0300

    create layout

---
+++ small_projects/small_to_do/src/routes/+layout.svelte
@@ -0,0 +1,15 @@
+
+<script lang="ts">
+	import type { Snippet } from 'svelte';
+  import '../app.css';
+
+  interface Props extends Record<string, unknown> {
+    children?: Snippet
+  }
+
+  let { children }: Props = $props();
+</script>
+
+<div class="container">
+  {@render children?.()}
+</div>
