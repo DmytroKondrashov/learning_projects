7b9891d4e0824e8e44f2eebc840f21c4d6784fd9
DmytroKondrashov <kondrashovd101@gmail.com>
2025-05-31 18:45:12 +0300

    simplify the query

--- small_projects/small_to_do/src/lib/queries/getAnimeList.ts
+++ small_projects/small_to_do/src/lib/queries/getAnimeList.ts
@@ -2,31 +2,9 @@ export const getAnimeList = `
 query getAnimeList($limit: Int, $page: Int) { 
   animes(limit: $limit, page: $page) {
     id
-    malId
     name
     russian
-    english
-    japanese
-    kind
-    rating
-    score
-    status
-    episodes
-    airedOn { year month day date }
-    releasedOn { year month day date }
     poster { id originalUrl mainUrl }
-
-    createdAt,
-    updatedAt,
-
-    genres { id name russian kind }
-    studios { id name imageUrl }
-
-    videos { id url name kind playerUrl imageUrl }
-    screenshots { id originalUrl x166Url x332Url }
-
-    description
-    descriptionHtml
   }
 }
 `;
