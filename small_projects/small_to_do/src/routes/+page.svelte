7c433f3a501953965ee8606847249bed2672d1e2
DmytroKondrashov <kondrashovd101@gmail.com>
2025-05-31 17:15:36 +0300

    run actual query

--- small_projects/small_to_do/src/routes/+page.svelte
+++ small_projects/small_to_do/src/routes/+page.svelte
@@ -2,18 +2,26 @@
   import urql from '$lib/urql';
   import { setContextClient, gql } from '@urql/svelte';
 	import { onMount } from 'svelte';
+  import { IntrospectionQuery } from '$lib/queries/IntrospectionQuery';
 
   setContextClient(urql);
 
   let todos = $state([]);
   let errors = $state([]);
+  let introspection = $state(null);
 
   onMount(async () => {
     console.log('onMount');
+    // const { data, error } = await urql.query(IntrospectionQuery, {}).toPromise();
+
+    // if (data) {
+    //   introspection = data;
+    // }
+
     const { data, error } = await urql.query(gql`
     query {
-      todo_list {
-        id
+      todo_listCollection {
+        name
       }
     }
   `, {}).toPromise();
@@ -48,9 +56,16 @@
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
 
 <ul>
-  {#each todos as todo}
-    <li>{todo.id}</li>
-  {/each}
+  {#if todos}
+    {#each todos as todo}
+      <li>{todo.id}</li>
+    {/each}
+    {#if todos.length === 0}
+      <li>No todos found</li>
+    {/if}
+  {:else}
+    <li>No todos found</li>
+  {/if}
 </ul>
 
 {#if errors.length > 0}
@@ -62,3 +77,5 @@
 {/if}
 
 <button onclick={fetchTodos}>Fetch Todos</button>
+
+<pre>{JSON.stringify(introspection, null, 2)}</pre>
