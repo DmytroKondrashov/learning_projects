e7ed5c22bf08236a003c9e2b943901c0be185748
DmytroKondrashov <kondrashovd101@gmail.com>
2025-05-31 17:38:39 +0300

    implement real query

--- small_projects/small_to_do/src/routes/+page.svelte
+++ small_projects/small_to_do/src/routes/+page.svelte
@@ -2,69 +2,39 @@
   import urql from '$lib/urql';
   import { setContextClient, gql } from '@urql/svelte';
 	import { onMount } from 'svelte';
-  import { IntrospectionQuery } from '$lib/queries/IntrospectionQuery';
+	import { getAnimeList } from '$lib/queries/getAnimeList';
 
   setContextClient(urql);
 
-  let todos = $state([]);
+  let animes = $state([]);
   let errors = $state([]);
   let introspection = $state(null);
 
   onMount(async () => {
     console.log('onMount');
-    // const { data, error } = await urql.query(IntrospectionQuery, {}).toPromise();
+    const { data, error } = await urql.query(getAnimeList, {}).toPromise();
 
-    // if (data) {
-    //   introspection = data;
-    // }
-
-    const { data, error } = await urql.query(gql`
-    query {
-      todo_listCollection {
-        name
-      }
-    }
-  `, {}).toPromise();
-
-    if (data) {
+    if (data?.animes) {
       console.log(data);
-      todos = data.todo_list;
+      animes = data.animes;
     }
 
-    if (error) {
+    if (error?.message) {
       errors.push(error.message);
     }
   });
-
-  async function fetchTodos() {
-    const { data, error } = await urql.query(gql`
-    query {
-      todo_list {
-        id
-      }
-    }
-  `, {}).toPromise();
-
-    if (data) {
-      console.log(data);
-      todos = data.todo_list;
-    }
-  }
 </script>
 
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
-
+<h1>Anime List</h1>
 <ul>
-  {#if todos}
-    {#each todos as todo}
-      <li>{todo.id}</li>
+  {#if animes}
+    {#each animes as anime}
+      <li>{anime.name}</li>
+    {:else}
+      <li>No animes found</li>
     {/each}
-    {#if todos.length === 0}
-      <li>No todos found</li>
-    {/if}
   {:else}
-    <li>No todos found</li>
+    <li>No animes found</li>
   {/if}
 </ul>
 
@@ -75,7 +45,3 @@
     {/each}
   </ul>
 {/if}
-
-<button onclick={fetchTodos}>Fetch Todos</button>
-
-<pre>{JSON.stringify(introspection, null, 2)}</pre>
