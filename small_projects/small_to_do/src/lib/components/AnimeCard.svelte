b01a4bbec27daed9d40e55f7bc9330c701aab1ae
DmytroKondrashov <kondrashovd101@gmail.com>
2025-05-31 18:09:59 +0300

    pass props to card

---
+++ small_projects/small_to_do/src/lib/components/AnimeCard.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	import type Anime from "$lib/interfaces/Anime";
+
+  let { anime }: { anime: Anime } = $props();
+</script>
+
+<div class="card">
+  <div class="card-image">
+    <figure class="image is-4by3">
+      <img
+        src={anime.poster.mainUrl}
+        alt="Placeholder image"
+      />
+    </figure>
+  </div>
+  <div class="card-content">
+    <div class="media">
+      <div class="media-content">
+        <p class="title is-4">{anime.name}</p>
+        <p class="subtitle is-6">{anime.russian}</p>
+      </div>
+    </div>
+
+    <div class="content" style:text-overflow="ellipsis">
+      {anime.description}
+    </div>
+  </div>
+</div>
