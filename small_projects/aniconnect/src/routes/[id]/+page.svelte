c3e00d4a5c1c62d4c188ed3c3a5219a44b9513fe
DmytroKondrashov <kondrashovd101@gmail.com>
2025-06-01 20:43:28 +0300

    work with different fields

--- small_projects/aniconnect/src/routes/[id]/+page.svelte
+++ small_projects/aniconnect/src/routes/[id]/+page.svelte
@@ -1,14 +1,41 @@
 <script lang="ts">
+	import type Anime from '$lib/interfaces/Anime';
   import type { PageData } from '../$types';
 
   let { data }: { data: PageData } = $props();
 </script>
 
+{#snippet valueFormatter(key: string, value: unknown)}
+  {#if key === 'genres'}
+    {@const genres = value as Anime['genres']}
+    {#each genres as { id, name, russian, kind } (id)}
+      <span>{name} - {russian} - {kind}</span>
+    {/each}
+  {:else if key === 'studios'}
+    {@const studios = value as Anime['studios']}
+    {studios.map(({ name }) => name).join(', ')}
+  {:else if key === 'videos'}
+    {@const videos = value as Anime['videos']}
+    {#each videos as { id, url, name, kind, playerUrl, imageUrl } (id)}
+      <span>{name} - {kind} - {playerUrl} - {imageUrl}</span>
+    {/each}
+  {:else if key === 'screenshots'}
+    {@const screenshots = value as Anime['screenshots']}
+    {#each screenshots as { id, originalUrl, x166Url, x332Url } (id)}
+      <span>{originalUrl} - {x166Url} - {x332Url}</span>
+    {/each}
+  {:else if key === 'descriptionHtml'}
+    <span>{@html value}</span>
+  {:else}
+    <span>{value}</span>
+  {/if}
+{/snippet}
+
 <dl>
   {#each Object.entries(data.anime) as [key, value]}
     {#if key !== 'poster'}
       <dt>{key}</dt>
-      <dd>{value}</dd>
+      <dd>{@render valueFormatter(key, value)}</dd>
     {/if}
   {/each}
 </dl>
