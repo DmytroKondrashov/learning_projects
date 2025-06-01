0a601507050b0088b5ef30b6949c73e4b401b800
DmytroKondrashov <kondrashovd101@gmail.com>
2025-06-01 20:15:04 +0300

    move everything to layout

--- small_projects/aniconnect/src/routes/[id]/+page.svelte
+++ small_projects/aniconnect/src/routes/[id]/+page.svelte
@@ -1,65 +1,14 @@
 <script lang="ts">
-	import { getAnime } from '$lib/queries/getAnime.js';
-	import urql from '$lib/urql.js';
-	import { onMount } from 'svelte';
+  import type { PageData } from '../$types';
 
-  let { data } = $props();
-
-  let loading = $state(false);
-  let errors = $state([]);
-  let anime = $state(null);
-  let { id } = data;
-
-  onMount(async () => {
-    loading = true;
-		const { data, error } = await urql.query(getAnime, { id }).toPromise();
-
-		if (data?.animes) {
-			anime = data?.animes[0];
-		}
-
-		if (error?.message) {
-			errors.push(error.message);
-		}
-
-		loading = false;
-  });
+  let { data }: { data: PageData } = $props();
 </script>
 
-<ul>
-	{#if loading}
-		<span>Loading...</span>
-	{:else if anime}
-  {@const content = Object.entries(anime)}
-  <div class="fixed-grid has-4-cols">
-		<div class="grid ">
-      <div class="cell">
-        <figure class="image">
-          <img
-            src={anime.poster.mainUrl}
-            alt="Placeholder image"
-          />
-        </figure>
-      </div>
-      <div class="cell is-col-span-3">
-        <dl>
-          {#each content as [key, value]}
-            <dt>{key}</dt>
-            <dd>{value}</dd>
-          {/each}
-        </dl>
-      </div>
-    </div>
-  </div>
-	{:else}
-		<span>Anime not found</span>
-	{/if}
-</ul>
-
-{#if errors.length > 0}
-	<ul>
-		{#each errors as error}
-			<li>{error}</li>
-		{/each}
-	</ul>
-{/if}
+<dl>
+  {#each Object.entries(data.anime) as [key, value]}
+    {#if key !== 'poster'}
+      <dt>{key}</dt>
+      <dd>{value}</dd>
+    {/if}
+  {/each}
+</dl>
