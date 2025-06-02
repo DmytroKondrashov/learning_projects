5b0338a6eb0b4770a9ba88b22c0897700cd5b5c9
DmytroKondrashov <kondrashovd101@gmail.com>
2025-06-02 22:28:09 +0300

    add new stores

--- small_projects/aniconnect/src/lib/stores.ts
+++ small_projects/aniconnect/src/lib/stores.ts
@@ -1,4 +1,6 @@
-import { writable } from 'svelte/store';
+import { readable, writable } from 'svelte/store';
 
 export const page = writable(1);
 export const limit = writable(12);
+export const limits = readable([12, 24, 36, 48]);
+export const search = writable('');
