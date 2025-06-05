7191ba3780553c06c7858ad1663400dd4eca2b3c
DmytroKondrashov <kondrashovd101@gmail.com>
2025-06-05 11:16:06 +0300

    fix size issue

--- small_projects/aniconnect/src/lib/components/FullVideo.svelte
+++ small_projects/aniconnect/src/lib/components/FullVideo.svelte
@@ -6,6 +6,30 @@
 <div class="modal" class:is-active={visible}>
   <div class="modal-background" {onclick}></div>
   <div class="modal-content is-full-height is-full-width">
-    <iframe {src} title={alt} loading="lazy" frameborder="0" allowfullscreen></iframe>
+    <div class="iframe-container">
+      <iframe {src} title={alt} loading="lazy" frameborder="0" allowfullscreen></iframe>
+    </div>
   </div>
 </div>
+
+<style>
+  .modal {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  .iframe-container {
+    width: 100%;
+    height: 50vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  iframe {
+    width: 100%;
+    height: 100%;
+    max-width: 800px;
+  }
+</style>
