e3042bb1fb1358d591f1a768594b8ab1af6b80fb
DmytroKondrashov <kondrashovd101@gmail.com>
2025-06-05 10:48:11 +0300

    logo update

--- small_projects/aniconnect/src/lib/components/Navbar.svelte
+++ small_projects/aniconnect/src/lib/components/Navbar.svelte
@@ -1,7 +1,7 @@
 <nav class="navbar" role="navigation" aria-label="main navigation">
 	<div class="navbar-brand">
 		<a class="navbar-item" href="/">
-			<img src="/aniconnect.png" alt="Aniconnect Logo" />
+			<img src="/aniconnect_logo.png" alt="Aniconnect Logo" />
 		</a>
 	</div>
 </nav>
