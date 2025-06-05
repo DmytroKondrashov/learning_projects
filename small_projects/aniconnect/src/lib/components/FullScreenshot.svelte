80b3d68d256d7d9e3e1d34af3ade962d7c2ddd49
DmytroKondrashov <kondrashovd101@gmail.com>
2025-06-05 20:32:48 +0300

    remove unneeded comparing

--- small_projects/aniconnect/src/lib/components/FullScreenshot.svelte
+++ small_projects/aniconnect/src/lib/components/FullScreenshot.svelte
@@ -2,16 +2,14 @@
   let { src = $bindable(), alt } = $props();
 </script>
 
-{#if src !== null}
-  <div class="modal" class:is-active={src !== null} onclick={() => src = null}>
-    <div class="modal-background"></div>
-    <div class="modal-content">
-      <p class="image">
-        <img {src} {alt} />
-      </p>
-    </div>
+<div class="modal" class:is-active={src !== null} onclick={() => src = null}>
+  <div class="modal-background"></div>
+  <div class="modal-content">
+    <p class="image">
+      <img {src} {alt} />
+    </p>
   </div>
-{/if}
+</div>
 
 <style>
 
