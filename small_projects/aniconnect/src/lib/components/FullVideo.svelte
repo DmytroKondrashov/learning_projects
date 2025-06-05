7b523243df7522cc88b92041b6df3c25e2567a13
DmytroKondrashov <kondrashovd101@gmail.com>
2025-06-05 10:51:53 +0300

    add full video component

---
+++ small_projects/aniconnect/src/lib/components/FullVideo.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  let { src, alt, visible = false, onclick } = $props();
+  console.log(src);
+</script>
+
+<div class="modal" class:is-active={visible}>
+  <div class="modal-background" {onclick}></div>
+  <div class="modal-content is-full-height is-full-width">
+    <iframe {src} title={alt} loading="lazy" frameborder="0" allowfullscreen></iframe>
+  </div>
+</div>
