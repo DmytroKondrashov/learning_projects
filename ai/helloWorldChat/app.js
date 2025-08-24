e45d67d7e5207cac43ca8a552c640c62368ac586
Dmytro Kondrashov <kondrashovd101@gmail.com>
2025-08-24 18:22:18 +0300

    include stream router

--- ai/helloWorldChat/app.js
+++ ai/helloWorldChat/app.js
@@ -5,7 +5,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var chatRouter = require('./routes/chat');
-
+var streamRouter = require('./routes/stream-chat');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
@@ -24,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/chat', chatRouter);
+app.use('/stream-chat', streamRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
