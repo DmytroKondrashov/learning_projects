02df3f3d2e57541d399ab009ebae14f42125569f
Dmytro Kondrashov <kondrashovd101@gmail.com>
2025-09-12 22:11:55 +0300

    add commented code to plot test images with predicted and true labels in Keras MNIST model

--- ai/learn_tensorflow/learn_keras.py
+++ ai/learn_tensorflow/learn_keras.py
@@ -103,3 +103,18 @@ def plot_value_array(i, predictions_array, true_label):
 # plt.subplot(1,2,2)
 # plot_value_array(i, predictions[i],  test_labels)
 # plt.show()
+
+
+# Plot the first X test images, their predicted labels, and the true labels.
+# Color correct predictions in blue and incorrect predictions in red.
+# num_rows = 5
+# num_cols = 3
+# num_images = num_rows*num_cols
+# plt.figure(figsize=(2*2*num_cols, 2*num_rows))
+# for i in range(num_images):
+#   plt.subplot(num_rows, 2*num_cols, 2*i+1)
+#   plot_image(i, predictions[i], test_labels, test_images)
+#   plt.subplot(num_rows, 2*num_cols, 2*i+2)
+#   plot_value_array(i, predictions[i], test_labels)
+# plt.tight_layout()
+# plt.show()
