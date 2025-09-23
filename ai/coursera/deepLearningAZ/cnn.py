8aa4939f23dee74a7e522a7aeb3b18d895ec21b1
Dmytro Kondrashov <kondrashovd101@gmail.com>
2025-09-23 22:19:38 +0300

    add initial CNN model structure and data preprocessing setup

---
+++ ai/coursera/deepLearningAZ/cnn.py
@@ -0,0 +1,6 @@
+import tensorflow as tf
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
+# ===============================
+# Part 1 - Data Preprocessing
+# ===============================
