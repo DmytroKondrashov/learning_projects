451abe88776700b82a234a452a8ca654f9663cd9
Dmytro Kondrashov <kondrashovd101@gmail.com>
2025-09-24 19:00:59 +0300

    add data augmentation techniques in CNN preprocessing with ImageDataGenerator

--- ai/coursera/deepLearningAZ/cnn.py
+++ ai/coursera/deepLearningAZ/cnn.py
@@ -4,3 +4,4 @@ from tensorflow.keras.preprocessing.image import ImageDataGenerator
 # ===============================
 # Part 1 - Data Preprocessing
 # ===============================
+train_datagen = ImageDataGenerator(rescale = 1./255, shear_range = 0.2, zoom_range = 0.2, horizontal_flip = True)
