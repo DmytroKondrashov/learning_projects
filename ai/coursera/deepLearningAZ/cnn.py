d7438981bf41bfe0173fef344e6913e84d1a1736
Dmytro Kondrashov <kondrashovd101@gmail.com>
2025-09-25 19:00:59 +0300

    update dataset directory paths in CNN data preprocessing to reflect new structure

--- ai/coursera/deepLearningAZ/cnn.py
+++ ai/coursera/deepLearningAZ/cnn.py
@@ -5,10 +5,10 @@ from tensorflow.keras.preprocessing.image import ImageDataGenerator
 # Part 1 - Data Preprocessing
 # ===============================
 train_datagen = ImageDataGenerator(rescale = 1./255, shear_range = 0.2, zoom_range = 0.2, horizontal_flip = True)
-training_set = train_datagen.flow_from_directory('/datasets/Part 2 - Convolutional Neural Networks (CNN)/training_set', target_size = (64, 64), batch_size = 32, class_mode = 'binary')
+training_set = train_datagen.flow_from_directory('datasets/Part 2 - Convolutional Neural Networks (CNN)/dataset/training_set', target_size = (64, 64), batch_size = 32, class_mode = 'binary')
 
 test_datagen = ImageDataGenerator(rescale = 1./255)
-test_set = test_datagen.flow_from_directory('/datasets/Part 2 - Convolutional Neural Networks (CNN)/test_set',
+test_set = test_datagen.flow_from_directory('datasets/Part 2 - Convolutional Neural Networks (CNN)/dataset/test_set',
                                             target_size = (64, 64),
                                             batch_size = 32,
                                             class_mode = 'binary')
