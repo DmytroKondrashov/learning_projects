fd673f8a8df3a78ddef5a040883e0b68ab9ac923
Dmytro Kondrashov <kondrashovd101@gmail.com>
2025-10-01 12:28:49 +0300

    add data preprocessing step by importing training set for RNN from Google stock price CSV

--- ai/coursera/deepLearningAZ/rnn.py
+++ ai/coursera/deepLearningAZ/rnn.py
@@ -2,4 +2,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
+# Part 1 - Data Preprocessing
+
+# Importing the training set
 dataset_train = pd.read_csv('datasets/Part 3 - Recurrent Neural Networks (RNN)/Google_Stock_Price_Train.csv')
+training_set = dataset_train.iloc[:, 1:2].values
+
