3c4012b387e772b5539f31d9a0376f0850f0342b
Dmytro Kondrashov <kondrashovd101@gmail.com>
2025-10-01 16:36:21 +0300

    implement feature scaling and create training data structure for RNN using 60 timesteps

--- ai/coursera/deepLearningAZ/rnn.py
+++ ai/coursera/deepLearningAZ/rnn.py
@@ -8,3 +8,15 @@ import pandas as pd
 dataset_train = pd.read_csv('datasets/Part 3 - Recurrent Neural Networks (RNN)/Google_Stock_Price_Train.csv')
 training_set = dataset_train.iloc[:, 1:2].values
 
+# Feature Scaling
+from sklearn.preprocessing import MinMaxScaler
+sc = MinMaxScaler(feature_range = (0, 1))
+training_set_scaled = sc.fit_transform(training_set)
+
+# Creating a data structure with 60 timesteps and 1 output
+X_train = []
+y_train = []
+for i in range(60, 1258):
+    X_train.append(training_set_scaled[i-60:i, 0])
+    y_train.append(training_set_scaled[i, 0])
+X_train, y_train = np.array(X_train), np.array(y_train)
