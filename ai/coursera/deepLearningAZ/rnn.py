0481c738084753b7d5c55b0a569791f91dd4b4e9
Dmytro Kondrashov <kondrashovd101@gmail.com>
2025-10-01 11:52:53 +0300

    add dataset loading for RNN by reading Google stock price data from CSV file

--- ai/coursera/deepLearningAZ/rnn.py
+++ ai/coursera/deepLearningAZ/rnn.py
@@ -2,3 +2,4 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
+dataset_train = pd.read_csv('datasets/Part 3 - Recurrent Neural Networks (RNN)/Google_Stock_Price_Train.csv')
