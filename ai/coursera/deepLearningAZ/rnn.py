dd462f8a9faa97b3baa81200b2c6f5ef6783af29
Dmytro Kondrashov <kondrashovd101@gmail.com>
2025-10-08 18:49:53 +0300

    Inverse transform predicted stock prices in rnn.py to convert scaled values back to original format for accurate visualization

--- ai/coursera/deepLearningAZ/rnn.py
+++ ai/coursera/deepLearningAZ/rnn.py
@@ -75,6 +75,7 @@ X_test = np.array(X_test)
 X_test = np.reshape(X_test, (X_test.shape[0], X_test.shape[1], 1))
 
 predicted_stock_price = regressor.predict(X_test)
+predicted_stock_price = sc.inverse_transform(predicted_stock_price)
 
 # Visualising the results
 plt.plot(real_stock_price, color = 'red', label = 'Real Google Stock Price')
