5ea7c92d6c18e8ebf8034650f8d4e120955db553
Dmytro Kondrashov <kondrashovd101@gmail.com>
2025-10-19 12:37:42 +0300

    Add feature to extract input and output variables from the dataset in som.py, enabling further processing for Self Organizing Maps.

--- ai/coursera/deepLearningAZ/som.py
+++ ai/coursera/deepLearningAZ/som.py
@@ -3,3 +3,5 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 dataset = pd.read_csv('datasets/Part 4 - Self Organizing Maps (SOM)/Credit_Card_Applications.csv')
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, -1].values
