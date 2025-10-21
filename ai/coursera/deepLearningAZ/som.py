fb122447d6911ca20865a4b55a70b7026e8b071c
Dmytro Kondrashov <kondrashovd101@gmail.com>
2025-10-21 16:52:12 +0300

    Add plotting of winning neurons in som.py to visualize Self Organizing Maps results, enhancing data interpretation.

--- ai/coursera/deepLearningAZ/som.py
+++ ai/coursera/deepLearningAZ/som.py
@@ -21,3 +21,13 @@ pcolor(som.distance_map().T)
 colorbar()
 markers = ['o', 's']
 colors = ['r', 'g']
+for i, x in enumerate(X):
+    w = som.winner(x)
+    plot(w[0] + 0.5,
+         w[1] + 0.5,
+         markers[y[i]],
+         markeredgecolor = colors[y[i]],
+         markerfacecolor = 'None',
+         markersize = 10,
+         markeredgewidth = 2)
+show()
