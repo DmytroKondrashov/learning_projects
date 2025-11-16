12f8b7756b21348ad0a8d22de87721a951615d31
Dmytro Kondrashov <kondrashovd101@gmail.com>
2025-11-16 11:16:34 +0200

    Add CategoryPieChart component for visualizing expenses by category. This component calculates the total expenses and displays them in a pie chart format, enhancing data visualization and user insights.

---
+++ fullstack/tauri/helloworld/src/lib/components/CategoryPieChart.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  import type { CategoryStats } from "$lib/types";
+
+  export let data: CategoryStats[]
+  export let title: string = 'Expenses by Category';
+
+  $: total = data.reduce((total, item)=> total + item.amount, 0)
+</script>
+
+
