831da6db25074192c2e93b4ab42d71335793869f
Dmytro Kondrashov <kondrashovd101@gmail.com>
2026-03-14 09:59:48 +0200

    refactor: move navigation to layout component and remove from individual pages

--- fullstack/re-learn_SvelteKit/src/routes/+layout.svelte
+++ fullstack/re-learn_SvelteKit/src/routes/+layout.svelte
@@ -1,9 +1,10 @@
 <script lang="ts">
-	import './layout.css';
-	import favicon from '$lib/assets/favicon.svg';
-
 	let { children } = $props();
 </script>
 
-<svelte:head><link rel="icon" href={favicon} /></svelte:head>
+<nav>
+	<a href="/">home</a>
+	<a href="/about">about</a>
+</nav>
+
 {@render children()}
