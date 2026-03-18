cc0fe803eba8c911662df978f77dd66271159f59
Dmytro Kondrashov <kondrashovd101@gmail.com>
2026-03-18 11:00:20 +0200

    feat: implement getTodos function to manage user-specific todo lists

--- fullstack/re-learn_SvelteKit/src/lib/server/database.js
+++ fullstack/re-learn_SvelteKit/src/lib/server/database.js
@@ -1,1 +1,13 @@
 const db = new Map();
+
+export function getTodos(userid) {
+	if (!db.get(userid)) {
+		db.set(userid, [{
+			id: crypto.randomUUID(),
+			description: 'Learn SvelteKit',
+			done: false
+		}]);
+	}
+
+	return db.get(userid);
+}
