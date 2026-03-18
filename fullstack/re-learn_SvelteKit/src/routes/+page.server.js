95f43fa9567b911e72baa8726b6fd6e0618ae356
Dmytro Kondrashov <kondrashovd101@gmail.com>
2026-03-18 11:33:40 +0200

    add default action

--- fullstack/re-learn_SvelteKit/src/routes/+page.server.js
+++ fullstack/re-learn_SvelteKit/src/routes/+page.server.js
@@ -16,3 +16,10 @@ export function load({ cookies }) {
     todos: db.getTodos(id)
 	};
 }
+
+export const actions = {
+	default: async ({ cookies, request }) => {
+		const data = await request.formData();
+		db.createTodo(cookies.get('userid'), data.get('description'));
+	}
+};
