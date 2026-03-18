0ed94643c82219f12a72c7ab406303fa30553299
Dmytro Kondrashov <kondrashovd101@gmail.com>
2026-03-18 12:50:38 +0200

    cosmetic updates

--- fullstack/re-learn_SvelteKit/src/routes/+page.svelte
+++ fullstack/re-learn_SvelteKit/src/routes/+page.svelte
@@ -1,4 +1,7 @@
 <script>
+  import { enhance } from '$app/forms';
+  import { fly, slide } from 'svelte/transition';
+
 	let { data, form } = $props();
 </script>
 
@@ -9,7 +12,7 @@
 		<p class="error">{form.error}</p>
 	{/if}
 
-  <form method="POST" action="?/create">
+  <form method="POST" action="?/create" use:enhance>
     <label>
       add a todo:
       <input
@@ -23,8 +26,8 @@
 
 	<ul class="todos">
 		{#each data.todos as todo (todo.id)}
-			<li>
-				<form method="POST" action="?/delete">
+			<li in:fly={{ y: 20 }} out:slide>
+				<form method="POST" action="?/delete" use:enhance>
           <input type="hidden" name="id" value={todo.id} />
           <span>{todo.description}</span>
           <button aria-label="Mark as complete"></button>
