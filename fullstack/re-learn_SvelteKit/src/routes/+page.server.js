0ddca1ac9a480f042caabc551b693ba03fddf86a
Dmytro Kondrashov <kondrashovd101@gmail.com>
2026-03-18 11:19:28 +0200

    update load function

--- fullstack/re-learn_SvelteKit/src/routes/+page.server.js
+++ fullstack/re-learn_SvelteKit/src/routes/+page.server.js
@@ -1,9 +1,18 @@
+import * as db from '$lib/server/database.js';
+
 export function load({ cookies }) {
 	const visited = cookies.get('visited');
+	let id = cookies.get('userid');
 
   cookies.set('visited', 'true', { path: '/' });
 
+  if (!id) {
+		id = crypto.randomUUID();
+		cookies.set('userid', id, { path: '/' });
+	}
+
 	return {
-		visited: visited === 'true'
+		visited: visited === 'true',
+    todos: db.getTodos(id)
 	};
 }
